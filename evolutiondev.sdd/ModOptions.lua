09d9d04f19e6b81ab7ca4ad7c9dee6631951c352
forbodingangel <forbodingangel@a8037ef8-a490-e903-bf31-6b53b2d07c0b>
2012-01-12 07:45:53 +0000

    Re-Adding Standard Mod Options (Allows setting of starting resources / Setting the game mode)
    git-svn-id: https://evolutionrts.googlecode.com/svn/trunk@191 a8037ef8-a490-e903-bf31-6b53b2d07c0b

--- evolutiondev.sdd/ModOptions.lua
+++ evolutiondev.sdd/ModOptions.lua
@@ -1,5 +1,74 @@
 local options= {
+  {
+    key    = 'StartingResources',
+    name   = 'Starting Resources',
+    desc   = 'Sets storage and amount of resources that players will start with',
+    type   = 'section',
+  },
 
+  {
+    key    = 'StartMetal',
+    name   = 'Starting metal',
+    desc   = 'Determines amount of metal and metal storage that each player will start with',
+    type   = 'number',
+    section= 'StartingResources',
+    def    = 100,
+    min    = 0,
+    max    = 100,
+    step   = 1,  -- quantization is aligned to the def value
+                    -- (step <= 0) means that there is no quantization
+  },
+  {
+   key    = 'StartMetal',
+   scope  = 'team',
+   name   = 'Team Starting metal',
+   desc   = 'Determines amount of metal and metal storage this team will start with',
+   type   = 'number',
+   section= 'StartingResources',
+   def    = 100,
+   min    = 0,
+   max    = 100,
+   step   = 1,  -- quantization is aligned to the def value
+   -- (step <= 0) means that there is no quantization
+  },
+  {
+    key    = 'StartEnergy',
+    name   = 'Starting energy',
+    desc   = 'Determines amount of energy and energy storage that each player will start with',
+    type   = 'number',
+    section= 'StartingResources',
+    def    = 250,
+    min    = 0,
+    max    = 250,
+    step   = 1,  -- quantization is aligned to the def value
+                    -- (step <= 0) means that there is no quantization
+  },
+  {
+   key    = 'StartEnergy',
+   scope  = 'team',
+   name   = 'Team Starting energy',
+   desc   = 'Determines amount of energy and energy storage that this team will start with',
+   type   = 'number',
+   section= 'StartingResources',
+   def    = 250,
+   min    = 0,
+   max    = 250,
+   step   = 1,  -- quantization is aligned to the def value
+   -- (step <= 0) means that there is no quantization
+  },
+	{
+		key="comm",
+		name="Game Mode",
+		desc="Choose the type of game",
+		type="list",
+		def="std",
+		items = {
+			{ key = "std", name = "Skirmish", desc = "Commander"  },
+			{ key = "sandbox", name = "Sandbox", desc = "Testing!" },
+			{ key = "feature", name = "Feature Placer", desc = "Not for gameplay, used for map development." }
+			--{ key = "con", name = "Standard Constructor", desc = "A regular T1 construction unit" }
+		},
+	},  
 }
 
 return options
