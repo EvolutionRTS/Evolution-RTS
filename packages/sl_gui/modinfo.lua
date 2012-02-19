6a6f0c26d45648da00269c17185d2221300334ed
rene.milk@gmail.com <rene.milk@gmail.com@a8037ef8-a490-e903-bf31-6b53b2d07c0b>
2012-02-19 17:11:00 +0000

    [sasi] bring back modinfo and springignore
    git-svn-id: https://evolutionrts.googlecode.com/svn/trunk@226 a8037ef8-a490-e903-bf31-6b53b2d07c0b

---
+++ packages/sl_gui/modinfo.lua
@@ -0,0 +1,12 @@
+return {
+  name='EvoRTS_Gui',
+  description='EvoRTS',
+  shortname='EvoRTS_Gui',
+  version='VERSION',
+  mutator='Official',
+  game='EvoRTS_Gui',
+  shortGame='EvoRTS_Gui',
+  modtype='T',
+  depend = { },
+}
+
