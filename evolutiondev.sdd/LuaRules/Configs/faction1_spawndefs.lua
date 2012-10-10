c2321c16b451c7e93f3664c8f2f6acbdf694ea39
forbodingangel <forbodingangel@a8037ef8-a490-e903-bf31-6b53b2d07c0b>
2012-10-10 00:52:24 +0000

    VERSION{v4.5}
    Remove mouse cursor resolution scaling (It was causing the mouse cursor to sometimes disappear)
    Decrease difficulty of Survival AI
    Fix Bomber targeting
    
    git-svn-id: https://evolutionrts.googlecode.com/svn/trunk@387 a8037ef8-a490-e903-bf31-6b53b2d07c0b

--- evolutiondev.sdd/LuaRules/Configs/faction1_spawndefs.lua
+++ evolutiondev.sdd/LuaRules/Configs/faction1_spawndefs.lua
@@ -2,7 +2,7 @@
 --------------------------------------------------------------------------------
 maxChicken           = tonumber(Spring.GetModOptions().mo_maxchicken) or 100
 maxBurrows           = 20
-gracePeriod          = tonumber(Spring.GetModOptions().mo_graceperiod) or 180  -- no chicken spawn in this period, seconds
+gracePeriod          = tonumber(Spring.GetModOptions().mo_graceperiod) or 300  -- no chicken spawn in this period, seconds
 queenTime            = (Spring.GetModOptions().mo_queentime or 40) * 60 -- time at which the queen appears, seconds
 addQueenAnger        = tonumber(Spring.GetModOptions().mo_queenanger) or 1
 burrowSpawnType      = Spring.GetModOptions().mo_chickenstart or "avoid"
@@ -19,7 +19,7 @@ burrowSpawnRate      = 60
 chickenSpawnRate     = 59
 minBaseDistance      = 1000      
 maxBaseDistance      = 7200
-chickensPerPlayer    = 8
+chickensPerPlayer    = 4
 spawnChance          = 0.5
 bonusTurret          = "eaaturret" -- Turret that gets spawned when a burrow dies
 angerBonus           = 204
