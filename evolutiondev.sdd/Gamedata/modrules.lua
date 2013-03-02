156233d2777d1c7000d9b45baa21531ae34707dc
forbodingangel <forbodingangel@a8037ef8-a490-e903-bf31-6b53b2d07c0b>
2013-03-02 05:15:41 +0000

    Update Modrules
    git-svn-id: https://evolutionrts.googlecode.com/svn/trunk@479 a8037ef8-a490-e903-bf31-6b53b2d07c0b

--- evolutiondev.sdd/Gamedata/modrules.lua
+++ evolutiondev.sdd/Gamedata/modrules.lua
@@ -11,35 +11,35 @@
 local modrules  = {
 
   -- system = {
-	-- pathFinderSystem = 1;
+	-- pathFinderSystem = 1,
   -- },
 
   movement = {
-    allowAirPlanesToLeaveMap = true;  -- defaults to true
-    allowPushingEnemyUnits   = true; -- defaults to false
-    allowCrushingAlliedUnits = false; -- defaults to false
-    allowUnitCollisionDamage = false; -- defaults to false
+    allowAirPlanesToLeaveMap = true,  -- defaults to true
+    allowPushingEnemyUnits   = true, -- defaults to false
+    allowCrushingAlliedUnits = false, -- defaults to false
+    allowUnitCollisionDamage = false, -- defaults to false
 	allowGroundUnitGravity	=	true,
   },
   
   construction = {
-    constructionDecay      = true; -- defaults to true
-    constructionDecayTime  = 0.01;  -- defaults to 6.66
-    constructionDecaySpeed = 0.01;  -- defaults to 0.03
+    constructionDecay      = true, -- defaults to true
+    constructionDecayTime  = 0.01,  -- defaults to 6.66
+    constructionDecaySpeed = 0.01,  -- defaults to 0.03
   },
 
 
   reclaim = {
-    multiReclaim  = 1;    -- defaults to 0
-    reclaimMethod = 0;    -- defaults to 1
-    unitMethod    = 0;    -- defaults to 1
+    multiReclaim  = 1,    -- defaults to 0
+    reclaimMethod = 0,    -- defaults to 1
+    unitMethod    = 0,    -- defaults to 1
 
-    unitEnergyCostFactor    = 0;  -- defaults to 0
-    unitEfficiency          = 0;  -- defaults to 1
-    featureEnergyCostFactor = 0;  -- defaults to 0
+    unitEnergyCostFactor    = 0,  -- defaults to 0
+    unitEfficiency          = 0,  -- defaults to 1
+    featureEnergyCostFactor = 0,  -- defaults to 0
 
-    allowEnemies  = false;  -- defaults to true
-    allowAllies   = false;  -- defaults to true
+    allowEnemies  = false,  -- defaults to true
+    allowAllies   = false,  -- defaults to true
   },
 
 
@@ -71,10 +71,10 @@ local modrules  = {
 
 
   transportability = {
-    transportGround = 1;   -- defaults to true
-    transportHover  = 1;   -- defaults to false
-    transportShip   = 0;  -- defaults to false
-    transportAir    = 0;  -- defaults to false
+    transportGround = 1,   -- defaults to true
+    transportHover  = 1,   -- defaults to false
+    transportShip   = 0,  -- defaults to false
+    transportAir    = 0,  -- defaults to false
   },
 
 
@@ -84,29 +84,29 @@ local modrules  = {
     -- 1: global coords, mobile
     -- 2: unit coords, mobile
     -- 3: unit coords, locked
-    defaultMode=0;
+    defaultMode=0,
   },
 
 
   experience = {
-    experienceMult = 1; -- defaults to 1.0
+    experienceMult = 1, -- defaults to 1.0
 
     -- these are all used in the following form:
     --   value = defValue * (1 + (scale * (exp / (exp + 1))))
-    powerScale  = 0;  -- defaults to 1.0
-    healthScale = 0;  -- defaults to 0.7
-    reloadScale = 0;  -- defaults to 0.4
+    powerScale  = 0,  -- defaults to 1.0
+    healthScale = 0,  -- defaults to 0.7
+    reloadScale = 0,  -- defaults to 0.4
   },
 
 
   fireAtDead = {
-    fireAtKilled   = false;  -- defaults to false
-    fireAtCrashing = false;   -- defaults to false
+    fireAtKilled   = false,  -- defaults to false
+    fireAtCrashing = false,   -- defaults to false
   },
 
 
   nanospray = {
-    allow_team_colors = true;  -- defaults to true
+    allow_team_colors = true,  -- defaults to true
   },
 
   featureLOS = {
@@ -115,7 +115,7 @@ local modrules  = {
     -- 2 - allyteam/gaia features always visible
     -- 3 - all features always visible
     -- default 3
-    featureVisibility = 3;
+    featureVisibility = 3,
   },
   
 }
