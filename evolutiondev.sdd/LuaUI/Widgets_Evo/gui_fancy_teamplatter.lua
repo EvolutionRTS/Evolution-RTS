8dea130887bcf66d8124e3434314f03d28f8b433
forbodingangel@gmail.com <forbodingangel@gmail.com@a8037ef8-a490-e903-bf31-6b53b2d07c0b>
2014-02-07 23:49:01 +0000

    Get rid of old playerlist Add new fancy teamplatter widget Add deluxe player list. Everything works except the energy storage bar. Probably has to do with ZK and overdrive.
    git-svn-id: https://evolutionrts.googlecode.com/svn/trunk@727 a8037ef8-a490-e903-bf31-6b53b2d07c0b

--- evolutiondev.sdd/LuaUI/Widgets_Evo/gui_fancy_teamplatter.lua
+++ evolutiondev.sdd/LuaUI/Widgets_Evo/gui_fancy_teamplatter.lua
@@ -15,8 +15,8 @@ function widget:GetInfo()
    return {
       name      = "Fancy Teamplatter",
       desc      = "Draws transparant smoothed donuts under friendly and enemy units (using teamcolor)",
-      author    = "TradeMark  (Floris: added multiple ally color support)",
-      date      = "17.03.2013",
+      author    = "TradeMark  (Floris: added multiple ally color support) (Forboding Angel: removed extra FX stuff) (zoggop: made it use only team colors)",
+      date      = "7.02.2014",
       license   = "GNU GPL, v2 or later",
       layer     = 5,
       enabled   = true  --  loaded by default?
@@ -30,7 +30,6 @@ end
 --------------------------------------------------------------------------------
 
 local drawWithHiddenGUI                 = true    -- keep widget enabled when graphical user interface is hidden (when pressing F5)
-local skipOwnAllyTeam                   = false    -- keep this 'true' if you dont want circles rendered under your own units
 
 local circleSize                        = 1
 local circleDivs                        = 12      -- how precise circle? octagon by default
@@ -38,11 +37,6 @@ local circleOpacity                     = 0.5
 local innerSize                         = 1.35    -- circle scale compared to unit radius
 local outerSize                         = 1.5    -- outer fade size compared to circle scale (1 = no outer fade)
                                         
-local defaultColorsForAllyTeams         = 0       -- (number of teams)   if number <= of total numebr of allyTeams then dont use teamcoloring but default colors
-local keepTeamColorsForSmallAllyTeam    = 16       -- (number of teams)   use teamcolors if number or teams (inside allyTeam)  <=  this value
-local spotterColor = {                            -- default color values
-   {0,0,1} , {1,0,1} , {0,1,1} , {0,1,0} , {1,0.5,0} , {0,1,1} , {1,1,0} , {1,1,1} , {0.5,0.5,0.5} , {0,0,0} , {0.5,0,0} , {0,0.5,0} , {0,0,0.5} , {0.5,0.5,0} , {0.5,0,0.5} , {0,0.5,0.5} , {1,0.5,0.5} , {0.5,0.5,0.1} , {0.5,0.1,0.5},
-}
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
@@ -64,23 +58,18 @@ local spGetTeamColor          = Spring.GetTeamColor
 local spGetUnitDefDimensions  = Spring.GetUnitDefDimensions
 local spGetUnitDefID          = Spring.GetUnitDefID
 local spIsUnitSelected        = Spring.IsUnitSelected
-local spGetAllyTeamList       = Spring.GetAllyTeamList 
 local spGetTeamList           = Spring.GetTeamList
 local spGetVisibleUnits       = Spring.GetVisibleUnits
 local spIsGUIHidden           = Spring.IsGUIHidden
-local spGetUnitAllyTeam       = Spring.GetUnitAllyTeam
+local spGetUnitTeam           = Spring.GetUnitTeam
                               
 local myTeamID                = Spring.GetLocalTeamID()
-local myAllyID                = Spring.GetMyAllyTeamID()
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
 local realRadii               = {}
 local circlePolys             = {}
-local allyToSpotterColor      = {}
-local allyToSpotterColorCount = 0
-local pickTeamColor           = false
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
@@ -88,75 +77,55 @@ local pickTeamColor           = false
 
 -- Creating polygons:
 function widget:Initialize()
-   local allyTeamList = spGetAllyTeamList()
-   local numberOfAllyTeams = #allyTeamList
-   for allyTeamListIndex = 1, numberOfAllyTeams do
-      local allyID                = allyTeamList[allyTeamListIndex]
-      if not skipOwnAllyTeam  or  (skipOwnAllyTeam  and  not (allyID == myAllyID))  then
-         allyToSpotterColorCount     = allyToSpotterColorCount+1
-         allyToSpotterColor[allyID]  = allyToSpotterColorCount
-         local usedSpotterColor      = spotterColor[allyToSpotterColorCount]
-         if defaultColorsForAllyTeams < numberOfAllyTeams-1 then
-            local teamList              = spGetTeamList(allyID)
-            for teamListIndex = 1, #teamList do
-               local teamID = teamList[teamListIndex]
-               if (teamListIndex == 1  and  #teamList <= keepTeamColorsForSmallAllyTeam) then     -- only check for the first allyTeam  (to be consistent with picking a teamcolor or default color, inconsistency could happen with different teamsizes)
-                  pickTeamColor = true
-               end
-               if pickTeamColor then
-                  -- pick the first team in the allyTeam and take the color from that one
-                  if (teamListIndex == 1) then
-                     local r,g,b,a       = spGetTeamColor(teamID)
-                     usedSpotterColor[1] = r
-                     usedSpotterColor[2] = g
-                     usedSpotterColor[3] = b
-                  end
-               end
+
+   local teamList = spGetTeamList()
+   local numberOfTeams = #teamList
+
+   for teamListIndex = 1, #teamList do
+      local teamID = teamList[teamListIndex]
+      local r,g,b,a       = spGetTeamColor(teamID)
+
+      circlePolys[teamID] = glCreateList(function()
+        glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)      -- disable layer blending
+         
+         -- colored inner circle:
+         glBeginEnd(GL.TRIANGLES, function()
+            local radstep = (2.0 * math.pi) / circleDivs
+            for i = 1, circleDivs do
+               local a1 = (i * radstep)
+               local a2 = ((i+1) * radstep)
+               --(fadefrom)
+               glColor(r, g, b, 0)
+               glVertex(0, 0, 0)
+               --(colorSet)
+               glColor(r, g, b, circleOpacity)
+               glVertex(math.sin(a1), 0, math.cos(a1))
+               glVertex(math.sin(a2), 0, math.cos(a2))
             end
-         end
+         end)
          
-         
-         circlePolys[allyID] = glCreateList(function()
-         
-           glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)      -- disable layer blending
-            
-            -- colored inner circle:
-            glBeginEnd(GL.TRIANGLES, function()
+         if (outerSize ~= 1) then
+            -- colored outer circle:
+            glBeginEnd(GL.QUADS, function()
                local radstep = (2.0 * math.pi) / circleDivs
                for i = 1, circleDivs do
                   local a1 = (i * radstep)
                   local a2 = ((i+1) * radstep)
-                  --(fadefrom)
-                  glColor(usedSpotterColor[1], usedSpotterColor[2], usedSpotterColor[3], 0)
-                  glVertex(0, 0, 0)
                   --(colorSet)
-                  glColor(usedSpotterColor[1], usedSpotterColor[2], usedSpotterColor[3], circleOpacity)
+                  glColor(r, g, b, circleOpacity)
                   glVertex(math.sin(a1), 0, math.cos(a1))
                   glVertex(math.sin(a2), 0, math.cos(a2))
+                  --(fadeto)
+                  glColor(r, g, b, 0)
+                  glVertex(math.sin(a2)*outerSize, 0, math.cos(a2)*outerSize)
+                  glVertex(math.sin(a1)*outerSize, 0, math.cos(a1)*outerSize)
                end
             end)
-            
-            if (outerSize ~= 1) then
-               -- colored outer circle:
-               glBeginEnd(GL.QUADS, function()
-                  local radstep = (2.0 * math.pi) / circleDivs
-                  for i = 1, circleDivs do
-                     local a1 = (i * radstep)
-                     local a2 = ((i+1) * radstep)
-                     --(colorSet)
-                     glColor(usedSpotterColor[1], usedSpotterColor[2], usedSpotterColor[3], circleOpacity)
-                     glVertex(math.sin(a1), 0, math.cos(a1))
-                     glVertex(math.sin(a2), 0, math.cos(a2))
-                     --(fadeto)
-                     glColor(usedSpotterColor[1], usedSpotterColor[2], usedSpotterColor[3], 0)
-                     glVertex(math.sin(a2)*outerSize, 0, math.cos(a2)*outerSize)
-                     glVertex(math.sin(a1)*outerSize, 0, math.cos(a1)*outerSize)
-                  end
-               end)
-            end
-         end)
-      end
+         end
+      end)
+
    end
+
 end
 
 function widget:Shutdown()
@@ -198,16 +167,14 @@ function widget:DrawWorldPreUnit()
    local visibleUnits = spGetVisibleUnits()
    if #visibleUnits then
       for i=1, #visibleUnits do
-         unitID = visibleUnits[i]
-         local allyID = spGetUnitAllyTeam(unitID)
-         if circlePolys[allyID] ~= nil then
-            if not skipOwnAllyTeam  or  (skipOwnAllyTeam  and  not (allyID == myAllyID))  then
-               local unitDefIDValue = spGetUnitDefID(unitID)
-               if (unitDefIDValue) then
-                  local radius = GetUnitDefRealRadius(unitDefIDValue) * circleSize
-                  if (radius) then
-                     glDrawListAtUnit(unitID, circlePolys[allyID], false, radius, 1.0, radius)
-                  end
+         local unitID = visibleUnits[i]
+         local teamID = spGetUnitTeam(unitID)
+         if circlePolys[teamID] ~= nil then
+            local unitDefIDValue = spGetUnitDefID(unitID)
+            if (unitDefIDValue) then
+               local radius = GetUnitDefRealRadius(unitDefIDValue) * circleSize
+               if (radius) then
+                  glDrawListAtUnit(unitID, circlePolys[teamID], false, radius, 1.0, radius)
                end
             end
          end
