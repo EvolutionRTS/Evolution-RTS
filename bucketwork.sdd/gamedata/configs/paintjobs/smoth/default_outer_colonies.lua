a6a2f90a07d589fefcb3de9ab5d797792e8d089a
smothness@gmail.com <smothness@gmail.com@a8037ef8-a490-e903-bf31-6b53b2d07c0b>
2014-04-06 05:44:57 +0000

    tweak to smoth theme base
    git-svn-id: https://evolutionrts.googlecode.com/svn/trunk@793 a8037ef8-a490-e903-bf31-6b53b2d07c0b

--- bucketwork.sdd/gamedata/configs/paintjobs/smoth/default_outer_colonies.lua
+++ bucketwork.sdd/gamedata/configs/paintjobs/smoth/default_outer_colonies.lua
@@ -1,25 +1,14 @@
 local colors	= {
-		paintR				= "white",
-		sheenR				= "gold_gloss",
-		--texture projection map setting
-		projectionGlmode1		= '',
-		projectionTexture1 		= '',
-		projectionMatrix1	= '',
-		
-		paintG				= "olivedrab",
-		sheenG				= "gold_gloss",
-		--texture projection map setting
-		projectionGlmode2		= '',
-		projectionTexture2 		= '',
-		projectionMatrix2	= '',
-		
-		paintB				= "imperialpurple",
-		sheenB				= "blue_gloss",
-		--does not have a texture projection map setting
-				
-		paintA				= "grey_20",
-		sheenA				= "gold_standard",
-		--does not have a texture projection map setting
+		sheenR		= "black_matte",
+		sheenG		= "black_matte",
+		sheenB		= "black_matte",
+		sheenA		= "black_matte",
+		paintR		= "olivedrab",
+		paintG		= "khaki",
+		paintB		= "light_mint",
+		paintA		= "red",
+		projectionMatrix1		= "scale10b",
+		projectionMatrix2		= "scale20",
+		----------------------------------
 }
-
 return colors
