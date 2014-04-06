dfd406339a76b074315026afc8b98e0b6122fc61
smothness@gmail.com <smothness@gmail.com@a8037ef8-a490-e903-bf31-6b53b2d07c0b>
2014-04-06 06:04:23 +0000

    fix for colors issue
    git-svn-id: https://evolutionrts.googlecode.com/svn/trunk@796 a8037ef8-a490-e903-bf31-6b53b2d07c0b

--- bucketwork.sdd/gamedata/configs/paintjobs/smoth/default_outer_colonies.lua
+++ bucketwork.sdd/gamedata/configs/paintjobs/smoth/default_outer_colonies.lua
@@ -1,14 +1,25 @@
 local colors	= {
-		sheenR		= "black_matte",
-		sheenG		= "black_matte",
-		sheenB		= "black_matte",
-		sheenA		= "black_matte",
-		paintR		= "olivedrab",
-		paintG		= "khaki",
-		paintB		= "light_mint",
-		paintA		= "red",
-		projectionMatrix1		= "scale10b",
-		projectionMatrix2		= "scale20",
-		----------------------------------
+		paintR				= "olivedrab",
+		sheenR				= "black_matte",
+		--texture projection map setting
+		projectionGlmode1		= '',
+		projectionTexture1 		= '',
+		projectionMatrix1	= '',
+		
+		paintG				= "khaki",
+		sheenG				= "black_matte",
+		--texture projection map setting
+		projectionGlmode2		= '',
+		projectionTexture2 		= '',
+		projectionMatrix2	= '',
+		
+		paintB				= "light_mint",
+		sheenB				= "black_matte",
+		--does not have a texture projection map setting
+				
+		paintA				= "red",
+		sheenA				= "black_matte",
+		--does not have a texture projection map setting
 }
+
 return colors
