9851ec2953187d38c885ab67d9683e0b7c3fba84
smothness@gmail.com <smothness@gmail.com@a8037ef8-a490-e903-bf31-6b53b2d07c0b>
2014-04-06 09:26:14 +0000

    Chickens update to my version. Variable spawn based on difficulty
    git-svn-id: https://evolutionrts.googlecode.com/svn/trunk@799 a8037ef8-a490-e903-bf31-6b53b2d07c0b

---
+++ bucketwork.sdd/gamedata/configs/survival_settings.lua
@@ -0,0 +1,24 @@
+local spawnerSettings	= {
+	smallUnit		= "elighttank3",--"aacar"
+	mediumUnit		= "eallterrmed",--"ez8"
+	largeUnit		= "eheavytank3",--xamel
+	referenceUnit	= "burrow",--fedmechist
+	
+	testUnit		= "burrow",
+	
+	modes			= {
+		[1]		= "Survival Spawner: Very Easy",
+		[2]		= "Survival Spawner: Easy",
+		[3]		= "Survival Spawner: Normal",
+		[4]		= "Survival Spawner: Hard",
+		[5]		= "Survival Spawner: Very Hard",
+		[6]		= "no description",
+	},
+	
+	-- commander modes which should trigger the disable of the survival spawner
+	badModes		= {
+		feature	= true,
+		sandbox	= true,
+	},
+}
+return spawnerSettings
