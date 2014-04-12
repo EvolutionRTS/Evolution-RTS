87848219b84d71b793365061a852d46e723a490f
forbodingangel@gmail.com <forbodingangel@gmail.com@a8037ef8-a490-e903-bf31-6b53b2d07c0b>
2014-04-12 09:57:18 +0000

    VERSION{v7.40}
    Revert imbaturret
    
    git-svn-id: https://evolutionrts.googlecode.com/svn/trunk@822 a8037ef8-a490-e903-bf31-6b53b2d07c0b

--- evolutiondev.sdd/Units/ebuilding/elightturret2.lua
+++ evolutiondev.sdd/Units/ebuilding/elightturret2.lua
@@ -19,9 +19,9 @@ local unitDef = {
   corpse             = "ammobox",
   description        = [[Anti-Raid Defense Platform (Anti-Light)
 Building
-62.5 Damage vs Light
-15.6 Damage vs Armored
-6.25 Damage vs Buildings]],
+125 Damage vs Light
+31.25 Damage vs Armored
+12.5 Damage vs Buildings]],
   energyStorage      = 0,
   energyUse          = 0,
   explodeAs          = "MEDIUM_BUILDING",
@@ -104,14 +104,14 @@ local weaponDefs = {
     craterBoost        = 0,
     craterMult         = 0,
     explosionGenerator = "custom:genericshellexplosion-medium-lightning",
-	energypershot      = 3.125,
+	energypershot      = 12.5,
     impulseBoost       = 0,
     impulseFactor      = 0,
 	interceptedByShieldType = 4,
     lineOfSight        = true,
     noSelfDamage       = true,
     range              = 600,
-    reloadtime         = 0.5,
+    reloadtime         = 1,
     WeaponType         = "LightningCannon",
     rgbColor           = "0.1 0.2 0.5",
     rgbColor2          = "0 0 1",
@@ -125,7 +125,7 @@ local weaponDefs = {
 	  damagetype		= "elightturret2",  
     },      
     damage = {
-      default            = 62.5,
+      default            = 125,
     },
   },
 }
