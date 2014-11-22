6ab64e08f13cc0b2644f0e7f2a981c52fa589d83
ForbodingAngel <forbodingangel@gmail.com>
2014-11-22 15:30:16 -0800

    VERSION{v8.22} Convert Koyote to paralyzer gunship

--- Units/eair/efighter.lua
+++ Units/eair/efighter.lua
@@ -30,14 +30,14 @@ local unitDef                    = {
 	category                     = "LIGHT VTOL",
 	collide                      = true,
 	corpse                       = "ammobox",
-	cruiseAlt                    = 400,
-	description                  = [[Air Superiority Fighter
+	cruiseAlt                    = 200,
+	description                  = [[Paralysis Gunship
 	Light
-	300 Damage vs Light/Armored/s
+	200 Damage vs Light/Armored/s
 	75 Damage vs Building/s
-
-	Requires +3 Power
-	Uses +3 Supply]],
+	
+	Requires +5 Power
+	Uses +5 Supply]],
 	energyMake                   = 0,
 	energyStorage                = 0,
 	energyUse                    = 0,
@@ -46,7 +46,7 @@ local unitDef                    = {
 	footprintZ                   = 5,
 	hoverAttack                  = true,
 	floater                      = true,
-	iconType                     = "aa",
+	iconType                     = "tankdestroyer",
 	idleAutoHeal                 = .5,
 	idleTime                     = 2200,
 	maxAcc                       = 0.25,
@@ -66,7 +66,7 @@ local unitDef                    = {
 	repairable		             = false,
 	selfDestructAs               = "mediumExplosionGenericWhite",
 	side                         = "ARM",
-	sightDistance                = 1500,
+	sightDistance                = 500,
 	smoothAnim                   = true,
 	sonarDistance                = 0,
 	turnRate                     = 1500,
@@ -96,16 +96,16 @@ local unitDef                    = {
 	weapons                      = {
 		[1]                      = {
 			def                  = "antiaircannon",
-			onlyTargetCategory   = "VTOL",
+			badTargetCategory    = "BUILDING VTOL",
 		},
 	},
 	customParams                 = {
 		--    needed_cover       = 2,
 		death_sounds             = "generic",
-		RequireTech              = "3 Power",
+		RequireTech              = "5 Power",
 		armortype                = "light",
 		nofriendlyfire	         = "1",
-		supply_cost              = 3,
+		supply_cost              = 5,
 		normalstex               = "unittextures/lego2skin_explorernormal.dds", 
 		buckettex                = "unittextures/lego2skin_explorerbucket.dds",
 		factionname	             = "outer_colonies",  
@@ -116,8 +116,8 @@ local unitDef                    = {
 --------------------------------------------------------------------------------
 -- Energy Per Shot Calculation is: dmg / 20 * ((aoe / 1000) + 1)
 
-local weapon1Damage              = 30
-local weapon1AOE				 = 25
+local weapon1Damage              = 20
+local weapon1AOE				 = 5
 
 local weaponDefs                 = {
 	antiaircannon                = {
@@ -130,7 +130,7 @@ local weaponDefs                 = {
 		coreThickness            = 0.4,
 		burnblow		         = true,
 		--	cegTag               = "mediumcannonweapon3",
-		duration                 = 0.05,
+		duration                 = 0.025,
 		energypershot            = weapon1Damage / 20 * ((weapon1AOE / 1000) + 1),
 		explosionGenerator       = "custom:genericshellexplosion-large-sparks-burn",
 		fallOffRate              = 1,
@@ -140,10 +140,12 @@ local weaponDefs                 = {
 		lineOfSight              = true,
 		minintensity             = "1",
 		name                     = "Laser",
-		range                    = 1500,
+		paralyzer		         = true,
+		paralyzetime	         = 2.5,
+		range                    = 500,
 		reloadtime               = 0.1,
 		WeaponType               = "LaserCannon",
-		rgbColor                 = "1 0.5 0",
+		rgbColor                 = "0.5 0.5 0.5",
 		rgbColor2                = "1 1 1",
 		soundTrigger             = true,
 		soundstart               = "tmediumtankfire.wav",
