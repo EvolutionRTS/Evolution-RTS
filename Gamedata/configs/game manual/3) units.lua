62319795f42fe173c68937b9dc4b3be957205cb0
ForbodingAngel <forbodingangel@gmail.com>
2015-06-20 13:38:02 -0700

    Change name then change it back

---
+++ Gamedata/configs/game manual/3) units.lua
@@ -0,0 +1,25 @@
+local topic = {
+	["details"] = {
+		sectionTitle = "Units",
+	},
+	["article"]	= {
+		{ --page
+			["details"] = {
+				pageTitle = "3rd page",
+			},
+			["sections"] = {-- sections
+				{
+					text	= "Black diamond ship, \n\n Sluggish but powerful, it's twin plasma miniguns certainly will brighten your day!",
+					textColor 	= {0.5,0.5,0.5,1.0},
+					fontShadow	= false,
+					thumbImage	= "bitmaps/ui/loadingscreen/black diamond.png",
+					backgroundColor	= {0.5,0.1,0.5,0.0},
+					backgroundImage1	= "bitmaps/ui/loadingscreen/rainbows.png",
+				},
+			}
+		},
+
+	}
+}
+
+return topic
