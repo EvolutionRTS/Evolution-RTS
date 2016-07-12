d9e20c35f713e349db7b062552598df68cb2a08c
ForbodingAngel <forbodingangel@gmail.com>
2016-07-11 21:53:19 -0700

    Add config for control victury non capturing units Add documentation to main gadget file

---
+++ LuaRules/Configs/cv_nonCapturingUnits.lua
@@ -0,0 +1,11 @@
+local nonCapturingUnits = {
+	"eairengineer",
+	"efighter",
+	--"escout",
+	"egunship2",
+	"etransport",
+	"edrone",
+	"ebomber",
+}
+
+return nonCapturingUnits
--- LuaRules/Gadgets/game_controlVictory.lua
+++ LuaRules/Gadgets/game_controlVictory.lua
@@ -10,15 +10,167 @@ function gadget:GetInfo()
 	}
 end
 
+--[[
+-------------------
+Before implementing this gadget, read this!!!
+This gadget relies on three parts:
+• control point config file which is located in luarules/configs/controlpoints/ , and it must have a filename of cv_<mapname>.lua. So, in the case of a map named "Iammas Prime -" with a version of "v01", then the name of my file would be "cv_Iammas Prime - v01.lua".
+• config placed in luarules/configs/ called cv_nonCapturingUnits.lua
+• modoptions
+
+The control point config is structured like this (cv_Iammas Prime - v01.lua):
+
+////
+
+return {
+	points = { 
+		[1] = {x = 4608, y = 0, z = 3048},
+		[2] = {x = 4265, y = 0, z = 1350},
+		[3] = {x = 4950, y = 0, z = 4786},
+		[4] = {x = 6641, y = 0, z = 858},
+		[5] = {x = 2574, y = 0, z = 5271},
+		[6] = {x = 2219, y = 0, z = 498},
+		[7] = {x = 6993, y = 0, z = 5616},
+	},
+}
+
+////
+
+The nonCapturingUnits.lua config file is structured like this:
+
+////
+
 local nonCapturingUnits = {
 	"eairengineer",
 	"efighter",
-	--"escout",
 	"egunship2",
 	"etransport",
 	"edrone",
 	"ebomber",
 }
+
+return nonCapturingUnits
+
+////
+
+Here are all of the modoptions in a neat copy pastable form... Place these modoptions in modoptions.lua in your base game folder:
+
+////
+
+	{
+		key="scoremode",
+		name="Scoring Mode (Control Victory Points)",
+		desc="Defines how the game is played",
+		type="list",
+		def="countdown",
+		section="gameplayoptions",
+		items={
+			{key="disabled", name="Disabled", desc="Disable Control Points as a victory condition."},
+			{key="countdown", name="Countdown", desc="A Control Point decreases all opponents' scores, zero means defeat."},
+			{key="tugowar", name="Tug of War", desc="A Control Point steals enemy score, zero means defeat."},
+			{key="multidomination", name="Domination", desc="Holding all Control Points will grant 1000 score, first to reach the score limit wins."},
+		}
+	},
+	{
+		key    = 'controlvictoryoptions',
+		name   = 'Control Victory Options',
+		desc   = 'Allows you to control at a granular level the individual options for Control Point Victory',
+		type   = 'section',
+	},
+	
+	{
+		key    = 'limitscore',
+		name   = 'Total Score',
+		desc   = 'Total score amount available.',
+		type   = 'number',
+		section= 'controlvictoryoptions',
+		def    = 3500,
+		min    = 500,
+		max    = 5000,
+		step   = 1,  -- quantization is aligned to the def value
+		-- (step <= 0) means that there is no quantization
+	},
+	{
+		key    = 'captureradius',
+		name   = 'Capture Radius',
+		desc   = 'Radius around a point in which to capture it.',
+		type   = 'number',
+		section= 'controlvictoryoptions',
+		def    = 500,
+		min    = 100,
+		max    = 1000,
+		step   = 25,  -- quantization is aligned to the def value
+		-- (step <= 0) means that there is no quantization
+	},
+		{
+		key    = 'capturetime',
+		name   = 'Capture Time',
+		desc   = 'Time to capture a point.',
+		type   = 'number',
+		section= 'controlvictoryoptions',
+		def    = 30,
+		min    = 1,
+		max    = 60,
+		step   = 1,  -- quantization is aligned to the def value
+		-- (step <= 0) means that there is no quantization
+	},
+		{
+		key    = 'capturebonus',
+		name   = 'Capture Bonus',
+		desc   = 'How much faster capture takes place by adding more units.',
+		type   = 'number',
+		section= 'controlvictoryoptions',
+		def    = 0.5,
+		min    = 0,
+		max    = 10,
+		step   = 0.1,  -- quantization is aligned to the def value
+		-- (step <= 0) means that there is no quantization
+	},
+		{
+		key    = 'decapspeed',
+		name   = 'De-Cap Speed',
+		desc   = 'Speed multiplier for neutralizing an enemy point.',
+		type   = 'number',
+		section= 'controlvictoryoptions',
+		def    = 3,
+		min    = 0.1,
+		max    = 10,
+		step   = 0.1,  -- quantization is aligned to the def value
+		-- (step <= 0) means that there is no quantization
+	},
+		{
+		key    = 'starttime',
+		name   = 'Start Time',
+		desc   = 'The time when capturing can start.',
+		type   = 'number',
+		section= 'controlvictoryoptions',
+		def    = 0,
+		min    = 0,
+		max    = 300,
+		step   = 1,  -- quantization is aligned to the def value
+		-- (step <= 0) means that there is no quantization
+	},
+		{
+		key    = 'dominationscoretime',
+		name   = 'Domination Score Time',
+		desc   = 'Time needed holding all points to score in multi domination.',
+		type   = 'number',
+		section= 'controlvictoryoptions',
+		def    = 30,
+		min    = 1,
+		max    = 60,
+		step   = 1,  -- quantization is aligned to the def value
+		-- (step <= 0) means that there is no quantization
+	},
+
+////
+
+
+That's all folks!!!
+-------------------
+]]--
+
+nonCapturingUnits = VFS.Include"LuaRules/Configs/cv_nonCapturingUnits.lua"
 
 --local pointMarker = FeatureDefNames.xelnotgawatchtower.id -- Feature marking a point- This doesn't do anything atm
 
