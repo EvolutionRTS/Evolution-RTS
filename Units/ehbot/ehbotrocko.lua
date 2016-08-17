c05ede6dfbae5a2afa468851625e3aa0233297ac
ForbodingAngel <forbodingangel@gmail.com>
2016-08-16 18:49:41 -0700

    Add hbot rocko

---
+++ Units/ehbot/ehbotrocko.lua
@@ -0,0 +1,188 @@
+-- UNITDEF -- ehbotthud --
+--------------------------------------------------------------------------------
+
+local unitName                   = "ehbotrocko"
+
+--------------------------------------------------------------------------------
+
+local power						 = [[6 power]]
+local armortype					 = [[light]]
+local supply					 = [[3]]
+
+local weapon1Damage              = 10
+local weapon1AOE				 = 150
+--local projectiles				 = 5
+--local burst						 = 10
+local energycosttofire			 = weapon1Damage / 10 * ((weapon1AOE / 1000) + 1)
+
+local function roundToFirstDecimal(energycosttofire)
+    return math.floor(energycosttofire*10 + 0.5)*0.1
+end
+
+local unitDef                    = {
+
+	--mobileunit 
+	transportbyenemy             = false;
+	--**
+
+	acceleration                 = 1,
+	brakeRate                    = 1,
+	buildCostEnergy              = 0,
+	buildCostMetal               = 27,
+	builder                      = false,
+	buildTime                    = 5,
+	canAttack                    = true,
+	cancollect                   = "1",
+	canGuard                     = true,
+	canHover                     = true,
+	canMove                      = true,
+	canPatrol                    = true,
+	canstop                      = "1",
+	category                     = "LIGHT NOTAIR RAID",
+	corpse                       = "ammobox",
+	description                  = [[Light Raider 
+Requires +]] .. power .. [[ and Uses +]] .. supply .. [[ Supply]],
+	energyMake                   = 0,
+	energyStorage                = 0,
+	energyUse                    = 0,
+	explodeAs                    = "mediumExplosionGenericRed",
+	footprintX                   = 3,
+	footprintZ                   = 3,
+	--highTrajectory		   		 = 2,
+	iconType                     = "raider",
+	idleAutoHeal                 = .5,
+	idleTime                     = 2200,
+	leaveTracks                  = false,
+	maxDamage                    = 200,
+	maxSlope                     = 26,
+	maxVelocity                  = 10,
+	maxReverseVelocity           = 2,
+	maxWaterDepth                = 10,
+	metalStorage                 = 0,
+	movementClass                = "HOVERHBOT3",
+	name                         = "Rocko",
+	noChaseCategory              = "VTOL",
+	objectName                   = "ehbotrocko.s3o",
+	script						 = "ehbotrocko_lus.lua",
+	radarDistance                = 0,
+	repairable		             = false,
+	selfDestructAs               = "mediumExplosionGenericRed",
+	side                         = "CORE",
+	sightDistance                = 600,
+	smoothAnim                   = true,
+	stealth			             = true,
+	seismicSignature             = 2,
+	--  turnInPlace              = false,
+	--  turnInPlaceSpeedLimit    = 5.5,
+	turnInPlace                  = true,
+	turnRate                     = 5000,
+	--  turnrate                 = 475,
+	unitname                     = "ehbotrocko",
+	upright                      = true,
+	workerTime                   = 0,
+
+	sfxtypes                     = {
+		explosiongenerators      = {
+			"custom:gdhcannon",
+			"custom:dirt",
+			"custom:blacksmoke",
+			"custom:airfactoryhtrail",
+		},
+		pieceExplosionGenerators = {
+			"deathceg3",
+			"deathceg4",
+		},	
+	},
+
+	sounds                       = {
+		underattack              = "unitsunderattack1",
+		ok                       = {
+			"ack",
+		},
+		select                   = {
+			"unitselect",
+		},
+	},
+	weapons                      = {
+		[1]                      = {
+			def                  = "rockets",
+--			mainDir = "0 0 1", -- x:0 y:0 z:1 => that's forward!
+--			maxAngleDif = 70,
+			badTargetCategory    = "VTOL ARMORED WALL",
+		},
+	},
+	customParams                 = {
+		canbetransported 		 = "true",
+		needed_cover             = 1,
+		death_sounds             = "generic",
+		RequireTech              = power,
+		armortype                = armortype,
+		nofriendlyfire	         = "1",
+		supply_cost              = supply,
+		normaltex               = "unittextures/lego2skin_explorernormal.dds", 
+		buckettex                = "unittextures/lego2skin_explorerbucket.dds",
+		factionname	             = "outer_colonies",  
+		helptext                 = [[Armortype: ]] ..armortype.. [[ 
+ 
+45 Damage vs Light/Armored
+60 Damage vs Building 
+ 
+Energy cost to fire: ]] .. roundToFirstDecimal(energycosttofire),
+	},
+}
+
+
+--------------------------------------------------------------------------------
+-- Energy Per Shot Calculation is: dmg / 20 * ((aoe / 1000) + 1)
+
+local weaponDefs                 = {
+	rockets             = {
+		AreaOfEffect             = weapon1AOE,
+		avoidFriendly            = false,
+		avoidFeature             = false,
+		collideFriendly          = false,
+		collideFeature           = false,
+		cegTag                   = "missiletrailflashy",
+		explosionGenerator       = "custom:genericshellexplosion-medium-red",
+		energypershot            = energycosttofire,
+		fireStarter              = 70,
+		tracks                   = true,
+		impulseFactor            = 0,
+		interceptedByShieldType  = 4,
+		model                    = "missilesmalllauncher.s3o",
+		name                     = "Rockets",
+		range                    = 700,
+		reloadtime               = 1,
+		weaponType		         = "MissileLauncher",
+		smokeTrail               = false,
+		soundStart               = "rocket1.wav",
+		soundHit                 = "explode5.wav",
+		startVelocity            = 250,
+		tolerance                = 10000,
+		turnrate                 = 7500,
+		predictBoost	         = 0.5,
+		turret                   = true,
+		weaponAcceleration       = 50,
+		flightTime               = 5,
+		trajectoryHeight         = 1.5,
+		sprayangle		         = 10000,
+		weaponVelocity           = 2000,
+		customparams             = {
+			damagetype		     = "eamphibrock",  
+
+			--Upgrades--
+			upgradeClass		 = "groundweapons",
+		},     
+		damage                   = {
+			default              = weapon1Damage,
+		},
+	},
+}
+unitDef.weaponDefs               = weaponDefs
+
+
+--------------------------------------------------------------------------------
+
+return lowerkeys({ [unitName]    = unitDef })
+
+--------------------------------------------------------------------------------
