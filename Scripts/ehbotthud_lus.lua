ff30d9725fafa348fe71c684b6c1a28bdf0e52f1
ForbodingAngel <forbodingangel@gmail.com>
2016-08-16 18:11:48 -0700

    Add hbot thud

---
+++ Scripts/ehbotthud_lus.lua
@@ -0,0 +1,72 @@
+base, turret, barrel1, firepoint1, firepoint2, dirt = piece('base', 'turret', 'barrel1', 'firepoint1', 'firepoint2', 'dirt')
+local SIG_AIM = {}
+
+-- state variables
+isMoving = "isMoving"
+terrainType = "terrainType"
+
+function script.Create()
+	StartThread(common.SmokeUnit, {base, turret, barrel1})
+	StartThread(doYouEvenLift)
+end
+
+common = include("headers/common_includes_lus.lua")
+
+function script.StartMoving()
+   isMoving = true
+   	StartThread(thrust)
+end
+
+function script.StopMoving()
+   isMoving = false
+end   
+
+function doYouEvenLift()
+	common.HbotLift()
+end
+
+function thrust()
+	common.DirtTrail()
+end
+
+local function RestoreAfterDelay()
+	Sleep(2000)
+	Turn(turret, y_axis, 0, 5)
+	Turn(barrel1, x_axis, 0, 5)
+end		
+
+function script.AimFromWeapon(weaponID)
+	--Spring.Echo("AimFromWeapon: FireWeapon")
+	return turret
+end
+
+local firepoints = {firepoint1, firepoint2}
+local currentFirepoint = 1
+
+function script.QueryWeapon(weaponID)
+	return firepoints[currentFirepoint]
+end
+
+function script.FireWeapon(weaponID)
+	currentFirepoint = 3 - currentFirepoint
+	EmitSfx (firepoints[currentFirepoint], 1024)
+end
+
+function script.AimWeapon(weaponID, heading, pitch)
+	Signal(SIG_AIM)
+	SetSignalMask(SIG_AIM)
+	Turn(turret, y_axis, heading, 100)
+	Turn(barrel1, x_axis, -pitch, 100)
+	WaitForTurn(turret, y_axis)
+	WaitForTurn(barrel1, x_axis)
+	StartThread(RestoreAfterDelay)
+	--Spring.Echo("AimWeapon: FireWeapon")
+	return true
+end
+
+function script.Killed()
+		Explode(barrel1, SFX.EXPLODE_ON_HIT)
+		Explode(turret, SFX.EXPLODE_ON_HIT)
+		Explode(base, SFX.EXPLODE_ON_HIT)
+		return 1   -- spawn ARMSTUMP_DEAD corpse / This is the equivalent of corpsetype = 1; in bos
+end
---
+++ Units/ehbot/ehbotthud.lua
@@ -0,0 +1,178 @@
+-- UNITDEF -- ehbotthud --
+--------------------------------------------------------------------------------
+
+local unitName                   = "ehbotthud"
+
+--------------------------------------------------------------------------------
+
+local power						 = [[6 power]]
+local armortype					 = [[light]]
+local supply					 = [[3]]
+
+local weapon1Damage              = 10
+local weapon1AOE				 = 150
+--local projectiles				 = 5
+--local burst						 = 10
+local energycosttofire			 = weapon1Damage / 10 * ((weapon1AOE / 1000) + 1)
+
+local function roundToFirstDecimal(energycosttofire)
+    return math.floor(energycosttofire*10 + 0.5)*0.1
+end
+
+local unitDef                    = {
+
+	--mobileunit 
+	transportbyenemy             = false;
+	--**
+
+	acceleration                 = 1,
+	brakeRate                    = 1,
+	buildCostEnergy              = 0,
+	buildCostMetal               = 27,
+	builder                      = false,
+	buildTime                    = 5,
+	canAttack                    = true,
+	cancollect                   = "1",
+	canGuard                     = true,
+	canHover                     = true,
+	canMove                      = true,
+	canPatrol                    = true,
+	canstop                      = "1",
+	category                     = "LIGHT NOTAIR RAID",
+	corpse                       = "ammobox",
+	description                  = [[Light Raider 
+Requires +]] .. power .. [[ and Uses +]] .. supply .. [[ Supply]],
+	energyMake                   = 0,
+	energyStorage                = 0,
+	energyUse                    = 0,
+	explodeAs                    = "mediumExplosionGeneric",
+	footprintX                   = 3,
+	footprintZ                   = 3,
+	highTrajectory		   		 = 2,
+	iconType                     = "raider",
+	idleAutoHeal                 = .5,
+	idleTime                     = 2200,
+	leaveTracks                  = false,
+	maxDamage                    = 200,
+	maxSlope                     = 26,
+	maxVelocity                  = 10,
+	maxReverseVelocity           = 2,
+	maxWaterDepth                = 10,
+	metalStorage                 = 0,
+	movementClass                = "HOVERHBOT3",
+	name                         = "Thud",
+	noChaseCategory              = "VTOL",
+	objectName                   = "ehbotthud.s3o",
+	script						 = "ehbotthud_lus.lua",
+	radarDistance                = 0,
+	repairable		             = false,
+	selfDestructAs               = "mediumExplosionGeneric",
+	side                         = "CORE",
+	sightDistance                = 600,
+	smoothAnim                   = true,
+	stealth			             = true,
+	seismicSignature             = 2,
+	--  turnInPlace              = false,
+	--  turnInPlaceSpeedLimit    = 5.5,
+	turnInPlace                  = true,
+	turnRate                     = 5000,
+	--  turnrate                 = 475,
+	unitname                     = "ehbotthud",
+	upright                      = true,
+	workerTime                   = 0,
+
+	sfxtypes                     = {
+		explosiongenerators      = {
+			"custom:gdhcannon",
+			"custom:dirt",
+			"custom:blacksmoke",
+			"custom:airfactoryhtrail",
+		},
+		pieceExplosionGenerators = {
+			"deathceg3",
+			"deathceg4",
+		},	
+	},
+
+	sounds                       = {
+		underattack              = "unitsunderattack1",
+		ok                       = {
+			"ack",
+		},
+		select                   = {
+			"unitselect",
+		},
+	},
+	weapons                      = {
+		[1]                      = {
+			def                  = "plasmacannon",
+--			mainDir = "0 0 1", -- x:0 y:0 z:1 => that's forward!
+--			maxAngleDif = 70,
+			badTargetCategory    = "VTOL ARMORED WALL",
+		},
+	},
+	customParams                 = {
+		canbetransported 		 = "true",
+		needed_cover             = 1,
+		death_sounds             = "generic",
+		RequireTech              = power,
+		armortype                = armortype,
+		nofriendlyfire	         = "1",
+		supply_cost              = supply,
+		normaltex               = "unittextures/lego2skin_explorernormal.dds", 
+		buckettex                = "unittextures/lego2skin_explorerbucket.dds",
+		factionname	             = "outer_colonies",  
+		helptext                 = [[Armortype: ]] ..armortype.. [[ 
+ 
+45 Damage vs Light/Armored
+60 Damage vs Building 
+ 
+Energy cost to fire: ]] .. roundToFirstDecimal(energycosttofire),
+	},
+}
+
+
+--------------------------------------------------------------------------------
+-- Energy Per Shot Calculation is: dmg / 20 * ((aoe / 1000) + 1)
+
+local weaponDefs                 = {
+	plasmacannon                	= {
+		accuracy			   = 500,
+		AreaOfEffect           = weapon1AOE,
+		avoidFriendly          = false,
+		avoidFeature 		   = false,
+		collideFriendly        = false,
+		collideFeature         = false,
+		--burst				   = burst,
+		--burstrate			   = 0.1,
+		explosionGenerator     = "custom:genericshellexplosion-small",
+		energypershot          = energycosttofire,
+		--duration			   = 0.25,
+		highTrajectory		   = 2,
+		name                   = "Plasma Cannon",
+		--noExplode			   = true,
+		range                  = 600,
+		reloadtime             = 0.25,
+		--projectiles			   = projectiles,
+		weaponType		       = "Cannon",
+		soundStart             = "bruisercannon.wav",
+		soundHit	           = "mediumcannonhit.wav",
+		soundTrigger           = true,
+		--sprayAngle             = 1000,
+		tolerance              = 10000,
+		turret                 = true,
+		weaponTimer            = 1,
+		weaponVelocity         = 600,
+		damage                   = {
+			default              = weapon1Damage,
+		},
+	},
+}
+unitDef.weaponDefs               = weaponDefs
+
+
+--------------------------------------------------------------------------------
+
+return lowerkeys({ [unitName]    = unitDef })
+
+--------------------------------------------------------------------------------
