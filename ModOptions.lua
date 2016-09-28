c1c1b558b00ac33b44d754e06f17dacb88752226
ForbodingAngel <forbodingangel@gmail.com>
2016-09-27 21:14:32 -0700

    Change scale of resources per point on CV

--- ModOptions.lua
+++ ModOptions.lua
@@ -168,8 +168,8 @@ local options= {
 		section= 'controlvictoryoptions',
 		def    = 0,
 		min    = 0,
-		max    = 20,
-		step   = 1,  -- quantization is aligned to the def value
+		max    = 1,
+		step   = 0.1,  -- quantization is aligned to the def value
 		-- (step <= 0) means that there is no quantization
 	},
 		{
