1aab1fc380341eff9c639c657c76d2c22f8068a6
Ruwetuin <floris@ruwhof.net>
2016-12-12 18:26:08 +0100

    spiced up loadscreen progress bar

--- LuaIntro/Addons/main.lua
+++ LuaIntro/Addons/main.lua
@@ -24,60 +24,42 @@ end
 
 local font = gl.LoadFont("FreeSansBold.otf", 50, 20, 1.95)
 
+
+function RectRound(px,py,sx,sy,cs)
+	--local px,py,sx,sy,cs = math.floor(px),math.floor(py),math.floor(sx),math.floor(sy),math.floor(cs)
+	
+	gl.Rect(px+cs, py, sx-cs, sy)
+	gl.Rect(sx-cs, py+cs, sx, sy-cs)
+	gl.Rect(px+cs, py+cs, px, sy-cs)
+	
+	gl.Texture(":n:luaui/Images/bgcorner.png")
+	gl.TexRect(px, py+cs, px+cs, py)		-- top left
+	gl.TexRect(sx, py+cs, sx-cs, py)		-- top right
+	gl.TexRect(px, sy-cs, px+cs, sy)		-- bottom left
+	gl.TexRect(sx, sy-cs, sx-cs, sy)		-- bottom right
+	gl.Texture(false)
+end
+
 function addon.DrawLoadScreen()
 	local loadProgress = SG.GetLoadProgress()
 
 	local vsx, vsy = gl.GetViewSizes()
-
+	
 	-- draw progressbar
 	local hbw = 3.5/vsx
 	local vbw = 3.5/vsy
 	local hsw = 0.2
 	local vsw = 0.2
-	gl.BeginEnd(GL.QUADS, function()
-
-		--bar bg
-		gl.Color(0,0,0,0.85)
-			gl.Vertex(0.2, 0.1)
-			gl.Vertex(0.8, 0.1)
-			gl.Vertex(0.8, 0.15)
-			gl.Vertex(0.2, 0.15)
-
-		--progress
-		gl.Color(1,0,0,0.3)
-			gl.Vertex(0.2, 0.1)
-			gl.Vertex(0.2 + math.max(0, loadProgress-0.01) * 0.6, 0.1)
-			gl.Vertex(0.2 + math.max(0, loadProgress-0.01) * 0.6, 0.15)
-			gl.Vertex(0.2, 0.15)
-		gl.Color(1,0.5,0.5,0.1)
-			gl.Vertex(0.2 + math.max(0, loadProgress-0.01) * 0.6, 0.1)
-			gl.Vertex(0.2 + math.max(0, loadProgress-0.01) * 0.6, 0.15)
-		gl.Color(1,0.25,0.25,0)
-			gl.Vertex(0.2 + math.min(1, math.max(0, loadProgress+0.01)) * 0.6, 0.15)
-			gl.Vertex(0.2 + math.min(1, math.max(0, loadProgress+0.01)) * 0.6, 0.1)
-
-		--bar borders
-		gl.Color(1,0,0,0.2) --Top Bar
-			gl.Vertex(0.2, 0.15)
-			gl.Vertex(0.8, 0.15)
-			gl.Vertex(0.8, 0.15-vbw)
-			gl.Vertex(0.2, 0.15-vbw)
-		gl.Color(1,0,0,0.2) --Bottom Bar
-			gl.Vertex(0.2, 0.10)
-			gl.Vertex(0.8, 0.10)
-			gl.Vertex(0.8, 0.10+vbw)
-			gl.Vertex(0.2, 0.10+vbw)
-		gl.Color(1,0,0,0.2) --Left Side Bar
-			gl.Vertex(0.2, 0.1)
-			gl.Vertex(0.2, 0.15)
-			gl.Vertex(0.2+hbw, 0.15)
-			gl.Vertex(0.2+hbw, 0.1)
-		gl.Color(1,0,0,0.2) --Right Side Bar
-			gl.Vertex(0.8, 0.1)
-			gl.Vertex(0.8, 0.15)
-			gl.Vertex(0.8-hbw, 0.15)
-			gl.Vertex(0.8-hbw, 0.1)
-	end)
+	
+	--bar bg
+	local paddingW = 0.005 * (vsy/vsx)
+	local paddingH = 0.005
+	gl.Color(0,0,0,0.85)
+	RectRound(0.2-paddingW,0.1-paddingH,0.8+paddingW,0.15+paddingH,0.004,0.035)
+	
+	gl.Color(1,loadProgress,0,0.25)
+	RectRound(0.2,0.1,0.2 + math.max(0, loadProgress-0.01) * 0.6,0.15,0.004,0.02)
+	
 
 --[[
 	gl.Color(0,0,0,1)
@@ -99,12 +81,13 @@ function addon.DrawLoadScreen()
 
 		--font:Print(lastLoadMessage, vsx * 0.5, vsy * 0.3, 50, "sc")
 		--font:Print(Game.gameName, vsx * 0.5, vsy * 0.95, vsy * 0.07, "sca")
-		font:Print(lastLoadMessage, vsx * 0.205, vsy * 0.14, barTextSize * 0.5, "sa")
+		font:Print(lastLoadMessage, vsx * 0.209, vsy * 0.134, barTextSize * 0.5, "sa")
 		if loadProgress>0 then
 			font:Print(("%.0f%%"):format(loadProgress * 100), vsx * 0.5, vsy * 0.115, barTextSize, "oc")
 		else
 			font:Print("Loading...", vsx * 0.5, vsy * 0.165, barTextSize, "oc")
 		end
+
 	gl.PopMatrix()
 end
 
