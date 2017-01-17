75171194336950e0b5b9604ce1933a9691298433
ForbodingAngel <forbodingangel@gmail.com>
2017-01-16 17:45:36 -0800

    Remove penetration power for AK shots

--- Units/ehbot/ehbotpeewee.lua
+++ Units/ehbot/ehbotpeewee.lua
@@ -149,7 +149,6 @@ local weaponDefs                 = {
 		impulseFactor          = 0,
 		interceptedByShieldType  = 4,
 		name                   = "E.M.G.",
-		noExplode			   = true,
 		range                  = weapon1Range,
 		reloadtime             = 1,
 		projectiles			   = projectiles,
