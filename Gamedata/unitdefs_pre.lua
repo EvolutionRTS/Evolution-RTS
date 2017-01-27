579205d5a2cc1dd4617184102a1bfa6a9f5a2748
sprunk <gnurps@o2.pl>
2017-01-27 21:44:48 +0100

    Avoid repetition

--- Gamedata/unitdefs_pre.lua
+++ Gamedata/unitdefs_pre.lua
@@ -22,35 +22,21 @@ Shared.buildList = {
 	[[eorb]],
 }
 
-Shared.buildListTurret = {
-	[[ehbotfac]],
-	[[eminifac]],
-	[[ebasefactory]],
-	[[eamphibfac]],
-	[[eairplant]],
-	[[etech1]],
-	[[esolar2]],
-	[[egeothermal]],
-	[[efusion2]],
-	[[estorage]],
-	[[eradar2]],
-	[[ejammer2]],
-	[[ekmar]],
-	[[elightturret2]],
-	[[eheavyturret2]],
-	[[eartyturret]],
-	[[euwturret]],
-	[[eshieldgen]],
-	[[esilo]],
-	[[emine]],
-	[[eorb]],
+if Spring.GetModOptions().luamex == "enabled" then
+	Shared.buildList [#Shared.buildList + 1] = [[emetalextractor]]
+end
+
+local turrets = {
 	[[ehbotpeewee_turret]],
 	[[ehbotthud_turret]],
 	[[ehbotsniper_turret]],
 	[[ehbotrocko_turret]],
-}
 
-if Spring.GetModOptions().luamex == "enabled" then
-	Shared.buildList [#Shared.buildList + 1] = [[emetalextractor]]
-	Shared.buildListTurret [#Shared.buildListTurret + 1] = [[emetalextractor]]
+}
+Shared.buildListTurret = {}
+for i = 1, #Shared.buildList do
+	Shared.buildListTurret[i] = Shared.buildList[i]
+end
+for i = 1, #turrets do
+	Shared.buildListTurret[#Shared.buildListTurret + 1] = turrets[i]
 end
