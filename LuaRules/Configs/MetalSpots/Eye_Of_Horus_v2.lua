1d423242e831cae87f905ac4cb27c1253481c99e
Forboding Angel <forbodingangel@gmail.com>
2018-04-15 00:57:14 -0700

    Remove unused cob buttons lua Add dynamic mex spots to eye of horus as a test, thanks to stowtemid for helping me

--- LuaRules/Configs/MetalSpots/Eye_Of_Horus_v2.lua
+++ LuaRules/Configs/MetalSpots/Eye_Of_Horus_v2.lua
@@ -3,19 +3,15 @@ local mapx, mapz = Game.mapSizeX * 0.5, Game.mapSizeZ * 0.5
 
 local size = math.min(mapx, mapz)
 local r = {size * 0.25, size * 0.5, size * 0.75, size * 0.95}
-local pointsPerLayer = {3, 8, 12, 8}
-local includeCentre = true
+local pointsPerLayer = {12, 10, 6, 12}
+local m = {1, 1.33, 1.66, 2}
 --
 local results = {}
 local lengResults = 1
-if includeCentre then
-    results[1] = {x = mapx, z = mapz, metal = 1}
-    lengResults = 2
-end
 for i = 1, #pointsPerLayer do
     for j = 1, pointsPerLayer[i] do
         local angle = math.pi * 2 * j / pointsPerLayer[i]
-        results[lengResults] = {x = math.round(mapx + r[i] * math.cos(angle)), z = math.round(mapz + r[i] * math.sin(angle)), metal = 1}
+        results[lengResults] = {x = math.round(mapx + r[i] * math.cos(angle)), z = math.round(mapz + r[i] * math.sin(angle)), metal = m[i]}
         lengResults = lengResults + 1
     end
 end
