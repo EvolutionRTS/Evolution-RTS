83308874b927704c8d960ff43b41932008297223
CommonPlayer <thedd5d2092d50@yahoo.com.hk>
2018-05-10 13:13:57 +0800

    factory queue persists through morphs

--- LuaUI/Widgets_Evo/keep_morphed_selected.lua
+++ LuaUI/Widgets_Evo/keep_morphed_selected.lua
@@ -2,24 +2,32 @@
 function widget:GetInfo()
 	return {
 		name = "Keep Morpheds Selected",
-		desc = "Maintain unit selection through morphing",
-		author = "zwzsg",
-		version = "1.0",
-		date = "August, 5th, 2009",
+		desc = "Maintain unit selection and factory queue (for EvoRTS) through morphing",
+		author = "zwzsg, modified by CommonPlayer",
+		version = "1.1",
+		date = "August, 5th, 2009", --modified by CommonPlayer, May 2018
 		license = "Public Domain",
 		layer = 0,
 		enabled = true,
 	}
 end
 
+local spGetFullBuildQueue = Spring.GetFullBuildQueue
+
 local Createds={}
 local Destroyeds={}
 
+local DestroyedsHasQueue={}
+
 function widget:UnitDestroyed(unitID,unitDefID,teamID)
+	local x,_,z=Spring.GetUnitPosition(unitID)
+	
 	if Spring.IsUnitSelected(unitID) then
-		local x,_,z=Spring.GetUnitPosition(unitID)
 		table.insert(Destroyeds,{u=unitID,x=x,z=z,t=teamID})
 	end
+	
+	local queue = spGetFullBuildQueue(unitID)
+	if queue[1] then table.insert(DestroyedsHasQueue, {u=unitID,x=x,z=z,t=teamID,queue=queue}) end
 end
 
 function widget:UnitCreated(unitID,unitDefID,teamID)
@@ -29,6 +37,30 @@ end
 
 function widget:GameFrame(frameNum)
 	local ToReSelect={}
+	
+	for _,c in ipairs(Createds) do
+		Spring.Echo("#DesQuee" .. #DestroyedsHasQueue)
+		for _,d in ipairs(DestroyedsHasQueue) do
+			if c.x==d.x and c.z==d.z and c.t==d.t then
+				for i = 1, #d.queue do
+					for k, v in pairs(d.queue[i]) do
+						local oldName = UnitDefs[k].name
+						local newName = oldName .. "_up1"
+						if oldName:find("_up1") then newName = oldName:gsub("_up1", "_up2")
+						elseif oldName:find("_up2") then newName = oldName:gsub("_up2", "_up3") end
+						local uDefName = UnitDefNames[newName]
+						if uDefName then
+							for _ = 1, v do Spring.GiveOrderToUnit(c.u, -uDefName.id, {}, 0) end
+							--Spring.Echo(UnitDefs[k].name .. "___" .. v)
+							Spring.Echo(uDefName.id)
+						end
+					end
+				end
+			end
+		end
+	end
+		
+		
 	for _,c in ipairs(Createds) do
 		for _,d in ipairs(Destroyeds) do
 			if c.x==d.x and c.z==d.z and c.t==d.t then
@@ -41,5 +73,6 @@ function widget:GameFrame(frameNum)
 	end
 	Createds={}
 	Destroyeds={}
+	
+	DestroyedsHasQueue={}
 end
-
