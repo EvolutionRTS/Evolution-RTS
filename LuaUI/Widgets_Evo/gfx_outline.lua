c2361bec31453ae7b28e94415d557debafbfe854
Floris <floris@ruwhof.net>
2018-05-20 12:32:50 +0200

    updated outline widget

--- LuaUI/Widgets_Evo/gfx_outline.lua
+++ LuaUI/Widgets_Evo/gfx_outline.lua
@@ -12,20 +12,27 @@
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
+local minFps = 13	-- disables below this average fps
+local fpsDiff = 4	-- enabled aain above minFps + fpsDiff
+
+local maxOutlineUnits = 750		-- ignores other units above this amount
+
 function widget:GetInfo()
   return {
     name      = "Outline",
-    desc      = "Displays a nice cartoon like outline around units.",
+    desc      = "Displays small outline around units (max "..maxOutlineUnits.."). Disables on low fps ("..minFps.."), re-enables on high fps ("..minFps+fpsDiff..")",
     author    = "jK",
     date      = "Dec 06, 2007",
     license   = "GNU GPL, v2 or later",
     layer     = -10,
-    enabled   = true  --  loaded by default?
+    enabled   = false  --  loaded by default?
   }
 end
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
+
+local customSize = 1
 
 --//textures
 local offscreentex
@@ -36,24 +43,23 @@ local blurtex
 local depthShader
 local blurShader_h
 local blurShader_v
-local uniformScreenXY, uniformScreenX, uniformScreenY
+local uniformScreenXY, uniformScreenX, uniformScreenY,uniformSizeX,uniformSizeY
 
 --// geometric
 local vsx, vsy = 0,0
 local resChanged = false
 
 --// display lists
-local enter2d,leave2d
+local enter2d,leave2d 
+
+local averageFps = minFps + fpsDiff + 6
+local show = true
+local outlineSize = 1
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-local GL_DEPTH_BITS = 0x0D56
-
-local GL_DEPTH_COMPONENT   = 0x1902
-local GL_DEPTH_COMPONENT16 = 0x81A5
 local GL_DEPTH_COMPONENT24 = 0x81A6
-local GL_DEPTH_COMPONENT32 = 0x81A7
 
 --// speed ups
 local ALL_UNITS       = Spring.ALL_UNITS
@@ -72,7 +78,6 @@ local glUseShader  = gl.UseShader
 local glUniform    = gl.Uniform
 local glUniformInt = gl.UniformInt
 
-local glBlending  = gl.Blending
 local glClear    = gl.Clear
 local glTexRect  = gl.TexRect
 local glColor    = gl.Color
@@ -87,8 +92,17 @@ local glPopMatrix     = gl.PopMatrix
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
+
+
 function widget:Initialize()
+
   vsx, vsy = widgetHandler:GetViewSizes()
+
+  if depthShader then
+    gl.DeleteShader(depthShader)
+    gl.DeleteShader(blurShader_h)
+    gl.DeleteShader(blurShader_v)
+  end
 
   depthShader = gl.CreateShader({
     fragment = [[
@@ -118,8 +132,7 @@ function widget:Initialize()
     fragment = [[
       uniform sampler2D tex0;
       uniform int screenX;
-
-      const vec2 kernel = vec2(0.6,0.7);
+      uniform float size;
 
       void main(void) {
         vec2 texCoord  = vec2(gl_TextureMatrix[0] * gl_TexCoord[0]);
@@ -129,7 +142,7 @@ function widget:Initialize()
         int n = 1;
         float pixelsize = 1.0/float(screenX);
         for(i = 1; i < 3; ++i){
-          gl_FragColor += kernel[n] * texture2D(tex0, vec2(texCoord.s + i*pixelsize,texCoord.t) );
+          gl_FragColor += size * texture2D(tex0, vec2(texCoord.s + i*pixelsize,texCoord.t) );
           --n;
         }
 
@@ -137,7 +150,7 @@ function widget:Initialize()
 
         n = 0;
         for(i = -2; i < 0; ++i){
-          gl_FragColor += kernel[n] * texture2D(tex0, vec2(texCoord.s + i*pixelsize,texCoord.t) );
+          gl_FragColor += size * texture2D(tex0, vec2(texCoord.s + i*pixelsize,texCoord.t) );
           ++n;
         }
       }
@@ -148,12 +161,10 @@ function widget:Initialize()
     },
   })
 
-
   blurShader_v = gl.CreateShader({
     fragment = [[      uniform sampler2D tex0;
       uniform int screenY;
-
-      const vec2 kernel = vec2(0.6,0.7);
+      uniform float size;
 
       void main(void) {
         vec2 texCoord  = vec2(gl_TextureMatrix[0] * gl_TexCoord[0]);
@@ -163,7 +174,7 @@ function widget:Initialize()
         int n = 1;
         float pixelsize = 1.0/float(screenY);
         for(i = 0; i < 2; ++i){
-          gl_FragColor += kernel[n] * texture2D(tex0, vec2(texCoord.s,texCoord.t + i*pixelsize) );
+          gl_FragColor += size * texture2D(tex0, vec2(texCoord.s,texCoord.t + i*pixelsize) );
           --n;
         }
 
@@ -171,7 +182,7 @@ function widget:Initialize()
 
         n = 0;
         for(i = -2; i < 0; ++i){
-          gl_FragColor += kernel[n] * texture2D(tex0, vec2(texCoord.s,texCoord.t + i*pixelsize) );
+          gl_FragColor += size * texture2D(tex0, vec2(texCoord.s,texCoord.t + i*pixelsize) );
           ++n;
         }
       }
@@ -183,24 +194,26 @@ function widget:Initialize()
   })
 
   if (depthShader == nil) then
-    Spring.Echo("Halo widget: depthcheck shader error: "..gl.GetShaderLog())
-    widgetHandler:RemoveWidget()
+    Spring.Echo("Outline widget: depthcheck shader error: "..gl.GetShaderLog())
+    widgetHandler:RemoveWidget(self)
     return false
   end
   if (blurShader_h == nil) then
-    Spring.Echo("Halo widget: hblur shader error: "..gl.GetShaderLog())
-    widgetHandler:RemoveWidget()
+    Spring.Echo("Outline widget: hblur shader error: "..gl.GetShaderLog())
+    widgetHandler:RemoveWidget(self)
     return false
   end
   if (blurShader_v == nil) then
-    Spring.Echo("Halo widget: vblur shader error: "..gl.GetShaderLog())
-    widgetHandler:RemoveWidget()
+    Spring.Echo("Outline widget: vblur shader error: "..gl.GetShaderLog())
+    widgetHandler:RemoveWidget(self)
     return false
   end
 
   uniformScreenXY = gl.GetUniformLocation(depthShader,  'screenXY')
   uniformScreenX  = gl.GetUniformLocation(blurShader_h, 'screenX')
+  uniformSizeX  = gl.GetUniformLocation(blurShader_h, 'size')
   uniformScreenY  = gl.GetUniformLocation(blurShader_v, 'screenY')
+  uniformSizeY  = gl.GetUniformLocation(blurShader_v, 'size')
 
   self:ViewResize(widgetHandler:GetViewSizes())
 
@@ -215,11 +228,28 @@ function widget:Initialize()
     glTexture(false)
     glUseShader(0)
   end)
+
+  WG['outline'] = {}
+  WG['outline'].getMaxunits = function()
+    return maxOutlineUnits
+  end
+  WG['outline'].setMaxunits = function(value)
+    maxOutlineUnits = value
+  end
+  WG['outline'].getSize = function()
+    return customSize
+  end
+  WG['outline'].setSize = function(value)
+    customSize = value
+    resChanged = true
+  end
 end
 
 function widget:ViewResize(viewSizeX, viewSizeY)
   vsx = viewSizeX
   vsy = viewSizeY
+
+  outlineSize = (0.85 + (vsx*vsy / 25000000))*0.55
 
   gl.DeleteTexture(depthtex or 0)
   gl.DeleteTextureFBO(offscreentex or 0)
@@ -270,6 +300,8 @@ function widget:Shutdown()
 
   gl.DeleteList(enter2d)
   gl.DeleteList(leave2d)
+
+  WG['outline'] = nil
 end
 
 
@@ -277,9 +309,12 @@ end
 --------------------------------------------------------------------------------
 
 local function DrawVisibleUnits()
-  local visibleUnits = GetVisibleUnits(ALL_UNITS,nil,true)
+  local visibleUnits = GetVisibleUnits(ALL_UNITS,nil,false)
+  local count = 0
   for i=1,#visibleUnits do  
     glUnit(visibleUnits[i],true)
+    count = count + 1
+    if count >= maxOutlineUnits then break end
   end
 end
 
@@ -305,8 +340,29 @@ local blur_v = function()
   glTexRect(-1-0.5/vsx,1+0.5/vsy,1+0.5/vsx,-1-0.5/vsy)
 end
 
+local sec = 0
+local lastUpdate = 0
+function widget:Update(dt)
+	sec = sec + dt
+	if sec > lastUpdate + 2 then
+		lastUpdate = sec
+
+		local modelCount = #Spring.GetVisibleUnits(-1,nil,false)
+		averageFps = averageFps + (((Spring.GetFPS()*(modelCount/50)) - averageFps*(modelCount/50)) / 70)
+		
+		if averageFps < minFps then
+			show = false
+		elseif averageFps > minFps + fpsDiff then
+			show = true
+		end
+	end
+end
+
+
 function widget:DrawWorldPreUnit()
-	glBlending(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA)
+  if not show then return end
+  gl.ResetState()		-- to prevent on/off flicker induced by other widgets maybe (i tried single out which thing included in gl.ResetSate fixed it but it kept doing it)
+ 	
   glCopyToTexture(depthtex,  0, 0, 0, 0, vsx, vsy)
   glTexture(depthtex)
 
@@ -314,16 +370,18 @@ function widget:DrawWorldPreUnit()
     resChanged = false
     if (vsx==1) or (vsy==1) then return end
     glUseShader(depthShader)
-    glUniform(uniformScreenXY,   vsx,vsy )
-     glUseShader(blurShader_h)
-    glUniformInt(uniformScreenX, vsx )
-     glUseShader(blurShader_v)
-    glUniformInt(uniformScreenY, vsy )
+     glUniform(uniformScreenXY,   vsx,vsy )
+    glUseShader(blurShader_h)
+     glUniformInt(uniformScreenX, vsx )
+     glUniform(uniformSizeX, outlineSize*customSize)
+    glUseShader(blurShader_v)
+     glUniformInt(uniformScreenY, vsy )
+     glUniform(uniformSizeY, outlineSize*customSize)
   end
-
+	
   glUseShader(depthShader)
   glRenderToTexture(offscreentex,MyDrawVisibleUnits)
-
+	
   glTexture(offscreentex)
   glRenderToTexture(blurtex, blur_h)
   glTexture(blurtex)
@@ -333,7 +391,19 @@ function widget:DrawWorldPreUnit()
   glTexture(offscreentex)
   glTexRect(-1-0.5/vsx,1+0.5/vsy,1+0.5/vsx,-1-0.5/vsy)
   glCallList(leave2d)
-	glBlending(false)
+end
+
+
+function widget:GetConfigData()
+  savedTable = {}
+  savedTable.maxOutlineUnits = maxOutlineUnits
+  savedTable.customSize = customSize
+  return savedTable
+end
+
+function widget:SetConfigData(data)
+  if data.maxOutlineUnits then maxOutlineUnits = data.maxOutlineUnits or maxOutlineUnits end
+  if data.customSize then customSize = data.customSize or customSize end
 end
 
 --------------------------------------------------------------------------------
