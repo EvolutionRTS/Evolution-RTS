7da6482b6f27041dc9e616d6072322a3365a0bfb
Damgam <c_2_damiano@op.pl>
2018-08-01 18:14:13 +0200

    added loading screen tips

--- LuaIntro/Addons/main.lua
+++ LuaIntro/Addons/main.lua
@@ -14,6 +14,11 @@ end
 
 ------------------------------------------
 
+local showTips = true
+if (Spring.GetConfigInt("LoadscreenTips",1) or 1) == 0 then
+	showTips = false
+end
+
 local lastLoadMessage = ""
 
 function addon.LoadProgress(message, replaceLastLine)
@@ -22,28 +27,134 @@ end
 
 ------------------------------------------
 
+-- Random tips we can show
+local tips = {
+	"PLACEHOLDER GAMEPLAY TIP #1",
+	"PLACEHOLDER GAMEPLAY TIP #2",
+}
+
+-- Random unit descriptions we can show
+local titleColor = "\255\215\255\215"
+local contentColor = "\255\255\255\255"
+local unit_descs = {
+	"ekrowloading.png "..titleColor.."Krow\n"..contentColor.."PLACEHOLDER UNIT TIP #1",
+	"ecommanderloading.png "..titleColor.."Overseer\n"..contentColor.."PLACEHOLDER UNIT TIP #2",
+}
+
+local quotes = {
+	{"The two most powerful warriors are patience and time.", "Leo Tolstoy"},
+	{"Know thy self, know thy enemy. A thousand battles, a thousand victories.", "Sun Tzu"},
+	{"People never lie so much as after a hunt, during a war or before an election.", "Otto von Bismarck"},
+	{"The best weapon against an enemy is another enemy.", "Friedrich Nietzsche"},
+	{"Thus, what is of supreme importance in war is to attack the enemy's strategy.", "Sun Tzu"},
+	{"Great is the guilt of an unnecessary war.", "John Adams"},
+	{"I have never advocated war except as a means of peace.", "Ulysses S Grant"},
+	{"War is not only a matter of equipment, artillery, group troops or air force; it is largely a matter of spirit, or morale.", "Chiang Kai-Shek"},
+	{"In nuclear war all men are cremated equal.", "Dexter Gordon"},
+	{"There are no absolute rules of conduct, either in peace or war. Everything depends on circumstances.", "Leon Trotsky"},
+	{"Weapons are an important factor in war, but not the decisive one; it is man and not materials that counts.", "Mao Zedong"},
+	{"To secure peace is to prepare for war.", "Carl von Clausewitz"},
+	{"Quickness is the essence of the war.", "Sun Tzu"},
+	{"The whole art of war consists of guessing at what is on the other side of the hill.", "Arthur Wellesley"},
+	{"War is a game that is played with a smile. If you can't smile, grin. If you can't grin, keep out of the way till you can.", "Winston Churchill"},
+	{"War can only be abolished through war, and in order to get rid of the gun it is necessary to take up the gun.", "Mao Zedong"},
+	{"The quickest way of ending a war is to lose it.", "George Orwell"},
+	{"Heaven cannot brook two suns, nor earth two masters.", "Alexander the Great"},
+	{"People always make war when they say they love peace.", "D H Lawrence"},
+	{"This is totally awesome. Wow. great job guys!!", "Chris Taylor"},
+	{"War is like love; it always finds a way.", "Bertolt Brecht"},
+	{"Ten soldiers wisely led will beat a hundred without a head.", "Euripides"},
+	{"In war there is no prize for runner-up.", "Lucius Annaeus Seneca"},
+	{"I think there should be holy war against yoga classes.", "Werner herzog"},
+	{"An army marches on its stomach.", "Napoleon Bonaparte"},
+	{"It is fatal to enter any war without the will to win it.", "Douglas MacArthur"},
+	{"You cannot simultaneously prevent and prepare for war.", "Albert Einstein"},
+	{"Try not to become a man of success, but rather try to become a man of value.", "Albert Einstein"},
+	{"Every failure is a step to success.", "William Whewell"},
+	{"If everyone is moving forward together, then success takes care of itself.", "Henry Ford"},
+	{"Failure is success if we learn from it.", "Malcolm Forbes"},
+	{"It is no use saying, 'We are doing our best.' You have got to succeed in doing what is necessary.", "Winston Churchill"},
+	{"Knowledge will give you power, but character respect.", "Bruce Lee"},
+	{"In time of war the laws are silent.", "Marcus Tullius Cicero"},
+	{"War is a contagion.", "Franklin D Roosevelt"},
+	{"War is the unfolding of miscalculations.", "Barbara Tuchman"},
+	{"Girl power is about loving yourself and having confidence and strength from within, so even if you're not wearing a sexy outfit, you feel sexy.", "Nicole Scherzinger"},
+	{"The most common way people give up their power is by thinking they don't have any.", "Alice Walker"},
+	{"Mastering others is strength. Mastering yourself is true power.", "Lao Tzu"},
+	{"There is more power in unity than division.", "Emmanuel Cleaver"},
+	{"I am not afraid of an army of lions led by a sheep; I am afraid of an army of sheep led by a lion.", "Alexander the Great"},
+	{"The power of an air force is terrific when there is nothing to oppose it.", "Winston Churchill"},
+	{"You must never underestimate the power of the eyebrow.", "Jack Black"},
+	{"Common sense is not so common.", "Voltaire"},
+	{"If everyone is thinking alike, then somebody isn't thinking.", "George S Patton"},
+	{"Ignorance is bold and knowledge reserved.", "Thucydides"},
+	{"Don't find fault, find a remedy.", "Henry Ford"},
+	{"There is nothing impossible to him who will try.", "Alexander the Great"},
+	{"Peace is produced by war", "Pierre Corneille"},
+}
+
+
+-- Since math.random is not random and always the same, we save a counter to a file and use that.
+filename = "LuaUI/Config/randomseed.data"
+k = os.time() % 1500
+if VFS.FileExists(filename) then
+	k = VFS.LoadFile(filename)
+end
+k = k + 1
+local file = assert(io.open(filename,'w'), "Unable to save latest randomseed from "..filename)
+    file:write(k)
+    file:close()
+file = nil
+
+local random_tip_or_desc = unit_descs[((k/2) % #unit_descs) + 1]
+if k%2 == 1 then
+	random_tip_or_desc = tips[((math.ceil(k/2)) % #tips) + 1]
+--elseif k%3 == 2 then
+--	random_tip_or_desc = quotes[((math.ceil(k/3)) % #quotes) + 1]
+end
+
+local loadedFontSize = 70
 local font = gl.LoadFont("FreeSansBold.otf", 70, 22, 1.15)
+
+local engineVersion = 100 -- just filled this in here incorrectly but old engines arent used anyway
+if Engine and Engine.version then
+	local function Split(s, separator)
+		local results = {}
+		for part in s:gmatch("[^"..separator.."]+") do
+			results[#results + 1] = part
+		end
+		return results
+	end
+	engineVersion = Split(Engine.version, '-')
+	if engineVersion[2] ~= nil and engineVersion[3] ~= nil then
+		engineVersion = tonumber(string.gsub(engineVersion[1], '%.', '')..engineVersion[2])
+	else
+		engineVersion = tonumber(Engine.version)
+	end
+elseif Game and Game.version then
+	engineVersion = tonumber(Game.version)
+end
 
 function DrawRectRound(px,py,sx,sy,cs)
 
-	local csx = cs
-	local csy = cs
-	if sx-px < (cs*2) then
-		csx = (sx-px)/2
-		if csx < 0 then csx = 0 end
-	end
-	if sy-py < (cs*2) then
-		csy = (sy-py)/2
-		if csy < 0 then csy = 0 end
-	end
-	cs = math.min(csx, csy)
+	--local csx = cs
+	--local csy = cs
+	--if sx-px < (cs*2) then
+	--	csx = (sx-px)/2
+	--	if csx < 0 then csx = 0 end
+	--end
+	--if sy-py < (cs*2) then
+	--	csy = (sy-py)/2
+	--	if csy < 0 then csy = 0 end
+	--end
+	--cs = math.min(cs, csy)
 
 	gl.TexCoord(0.8,0.8)
 	gl.Vertex(px+cs, py, 0)
 	gl.Vertex(sx-cs, py, 0)
 	gl.Vertex(sx-cs, sy, 0)
 	gl.Vertex(px+cs, sy, 0)
-	
+
 	gl.Vertex(px, py+cs, 0)
 	gl.Vertex(px+cs, py+cs, 0)
 	gl.Vertex(px+cs, sy-cs, 0)
@@ -103,6 +214,7 @@ function RectRound(px,py,sx,sy,cs)
 	--local px,py,sx,sy,cs = math.floor(px),math.floor(py),math.ceil(sx),math.ceil(sy),math.floor(cs)
 	
 	gl.Texture(":n:luaui/Images/bgcorner.png")
+	--gl.Texture(":n:luaui/Images/bgcorner.png")
 	gl.BeginEnd(GL.QUADS, DrawRectRound, px,py,sx,sy,cs)
 	gl.Texture(false)
 end
@@ -116,63 +228,156 @@ function gradienth(px,py,sx,sy, c1,c2)
 	gl.Vertex(px, sy, 0)
 end
 
+
+local lastLoadMessage = ""
+local lastProgress = {0, 0}
+
+local progressByLastLine = {
+	["Parsing Map Information"] = {0, 20},
+	["Loading Weapon Definitions"] = {10, 50},
+	["Loading LuaRules"] = {40, 80},
+	["Loading LuaUI"] = {70, 95},
+	["Finalizing"] = {100, 100}
+}
+for name,val in pairs(progressByLastLine) do
+	progressByLastLine[name] = {val[1]*0.01, val[2]*0.01}
+end
+
+function addon.LoadProgress(message, replaceLastLine)
+	lastLoadMessage = message
+	if message:find("Path") then -- pathing has no rigid messages so cant use the table
+		lastProgress = {0.8, 1.0}
+	end
+	lastProgress = progressByLastLine[message] or lastProgress
+end
+
 function addon.DrawLoadScreen()
 	local loadProgress = SG.GetLoadProgress()
+	if loadProgress == 0 then
+		loadProgress = lastProgress[1]
+	else
+		loadProgress = math.min(math.max(loadProgress, lastProgress[1]), lastProgress[2])
+	end
 
 	local vsx, vsy = gl.GetViewSizes()
-	
+
 	-- draw progressbar
 	local hbw = 3.5/vsx
 	local vbw = 3.5/vsy
 	local hsw = 0.2
 	local vsw = 0.2
-	
+	local yPos =  0.125 --0.054
+	local yPosTips = yPos + 0.1245
 	local loadvalue = 0.2 + (math.max(0, loadProgress) * 0.6)
-	
+
+	if not showTips then
+		yPos = 0.165
+		yPosTips = yPos
+	end
+
 	--bar bg
 	local paddingH = 0.004
 	local paddingW = paddingH * (vsy/vsx)
-	gl.Color(0.06,0.06,0.06,0.8)
-	RectRound(0.2-paddingW,0.1-paddingH,0.8+paddingW,0.15+paddingH,0.007)
-	
-	-- loadvalue
-	gl.Color(0.4-(loadProgress/7),loadProgress*0.4,0,0.4)
-	RectRound(0.2,0.1,loadvalue,0.15,0.0055)
-	
-	-- loadvalue gradient
-	gl.Texture(false)
-	gl.BeginEnd(GL.QUADS, gradienth, 0.2,0.1,loadvalue,0.15, {1-(loadProgress/3)+0.2,loadProgress+0.2,0+0.08,0.14}, {0,0,0,0.14})
-	
-	-- loadvalue inner glow
-	gl.Color(1-(loadProgress/3.5)+0.15,loadProgress+0.15,0+0.05,0.085)
-	gl.Texture(":n:luaui/Images/barglow-center.dds")
-	gl.TexRect(0.2,0.1,loadvalue,0.15)
-	
-	-- loadvalue glow
-	local glowSize = 0.045
-	gl.Color(1-(loadProgress/3)+0.15,loadProgress+0.15,0+0.05,0.07)
-	gl.Texture(":n:luaui/Images/barglow-center.dds")
-	gl.TexRect(0.2,	0.1-glowSize,	loadvalue,	0.15+glowSize)
-	
-	gl.Texture(":n:luaui/Images/barglow-edge.dds")
-	gl.TexRect(0.2-(glowSize*1.3), 0.1-glowSize, 0.2, 0.15+glowSize)
-	gl.TexRect(loadvalue+(glowSize*1.3), 0.1-glowSize, loadvalue, 0.15+glowSize)
-	
+	gl.Color(0.085,0.085,0.085,0.925)
+	RectRound(0.2-paddingW,yPos-0.05-paddingH,0.8+paddingW,yPosTips+paddingH,0.007)
+
+	gl.Color(0,0,0,0.75)
+	RectRound(0.2-paddingW,yPos-0.05-paddingH,0.8+paddingW,yPos+paddingH,0.007)
+
+    if loadvalue > 0.215 then
+	    -- loadvalue
+        gl.Color(0.4-(loadProgress/7),loadProgress*0.4,0,0.4)
+        RectRound(0.2,yPos-0.05,loadvalue,yPos,0.0055)
+
+        -- loadvalue gradient
+        gl.Texture(false)
+        gl.BeginEnd(GL.QUADS, gradienth, 0.2,yPos-0.05,loadvalue,yPos, {1-(loadProgress/3)+0.2,loadProgress+0.2,0+0.08,0.14}, {0,0,0,0.14})
+
+        -- loadvalue inner glow
+        gl.Color(1-(loadProgress/3.5)+0.15,loadProgress+0.15,0+0.05,0.04)
+        gl.Texture(":n:luaui/Images/barglow-center.png")
+        gl.TexRect(0.2,yPos-0.05,loadvalue,yPos)
+
+        -- loadvalue glow
+        local glowSize = 0.06
+        gl.Color(1-(loadProgress/3)+0.15,loadProgress+0.15,0+0.05,0.1)
+        gl.Texture(":n:luaui/Images/barglow-center.png")
+        gl.TexRect(0.2,	yPos-0.05-glowSize,	loadvalue,	yPos+glowSize)
+
+        gl.Texture(":n:luaui/Images/barglow-edge.png")
+        gl.TexRect(0.2-(glowSize*1.3), yPos-0.05-glowSize, 0.2, yPos+glowSize)
+        gl.TexRect(loadvalue+(glowSize*1.3), yPos-0.05-glowSize, loadvalue, yPos+glowSize)
+    end
+
 	-- progressbar text
 	gl.PushMatrix()
-	gl.Scale(1/vsx,1/vsy,1)
+		gl.Scale(1/vsx,1/vsy,1)
 		local barTextSize = vsy * 0.026
 
 		--font:Print(lastLoadMessage, vsx * 0.5, vsy * 0.3, 50, "sc")
 		--font:Print(Game.gameName, vsx * 0.5, vsy * 0.95, vsy * 0.07, "sca")
-		font:Print(lastLoadMessage, vsx * 0.21, vsy * 0.133, barTextSize * 0.67, "oa")
+		font:Print(lastLoadMessage, vsx * 0.21, vsy * (yPos-0.017), barTextSize * 0.67, "oa")
 		if loadProgress>0 then
-			font:Print(("%.0f%%"):format(loadProgress * 100), vsx * 0.5, vsy * 0.1175, barTextSize, "oc")
+			font:Print(("%.0f%%"):format(loadProgress * 100), vsx * 0.5, vsy * (yPos-0.0325), barTextSize, "oc")
 		else
-			font:Print("Loading...", vsx * 0.5, vsy * 0.165, barTextSize, "oc")
+			font:Print("Loading...", vsx * 0.5, vsy * (yPos-0.031), barTextSize, "oc")
+		end
+	gl.PopMatrix()
+
+
+	if showTips then
+		-- In this format, there can be an optional image before the tip/description.
+		-- Any image ends in .dss, so if such a text piece is found, we extract that and show it as an image.
+		local text_to_show = random_tip_or_desc
+		yPos = yPosTips
+		if random_tip_or_desc[2] then
+			text_to_show = random_tip_or_desc[1]
+		else
+			i, j = string.find(random_tip_or_desc, ".png")
+		end
+		local numLines = 1
+		local image_text = nil
+		local fontSize = barTextSize * 0.77
+		local image_size = 0.0485
+		local height = 0.123
+
+		if i ~= nil then
+			text_to_show = string.sub(text_to_show, j+2)
+			local maxWidth = ((0.58-image_size-0.012) * vsx) * (loadedFontSize/fontSize)
+			text_to_show, numLines = font:WrapText(text_to_show, maxWidth)
+		else
+			local maxWidth = (0.585 * vsx) * (loadedFontSize/fontSize)
+			text_to_show, numLines = font:WrapText(text_to_show, maxWidth)
 		end
 
-	gl.PopMatrix()
+		-- Tip/unit description
+		-- Background
+		--gl.Color(1,1,1,0.033)
+		--RectRound(0.2,yPos-height,0.8,yPos,0.005)
+
+		-- Text
+		gl.PushMatrix()
+		gl.Scale(1/vsx,1/vsy,1)
+
+		if i ~= nil then
+			image_text = string.sub(random_tip_or_desc, 0, j)
+			gl.Texture(":n:unitpics/loadingpics/" .. image_text)
+			gl.Color(1.0,1.0,1.0,0.8)
+			if (engineVersion < 1000 and engineVersion >= 105) or engineVersion > 10401151 then
+				gl.TexRect(vsx * 0.21, vsy*(yPos-0.015), vsx*(0.21+image_size), (vsy*(yPos-0.015))-(vsx*image_size),false,true)
+			else
+				gl.TexRect(vsx * 0.21, vsy*(yPos-0.015), vsx*(0.21+image_size), (vsy*(yPos-0.015))-(vsx*image_size))
+			end
+			font:Print(text_to_show, vsx * (0.21+image_size+0.012) , vsy * (yPos-0.0175), fontSize, "oa")
+		else
+			font:Print(text_to_show, vsx * 0.21, vsy * (yPos-0.0175), fontSize, "oa")
+		end
+
+		if random_tip_or_desc[2] then
+			font:Print('\255\255\222\155'..random_tip_or_desc[2], vsx * 0.79, (vsy * ((yPos-0.0175)-height)) +(fontSize*2.66) , fontSize, "oar")
+		end
+		gl.PopMatrix()
+	end
 end
 
 
