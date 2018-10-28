19b3e43bf639406464a27fce3c762d4a641854a9
Forboding Angel <forbodingangel@gmail.com>
2018-10-27 23:20:29 -0700

    Preliminary ideas for hotkeys

--- LuaUI/Configs/evo_buildHotkeysConfig.lua
+++ LuaUI/Configs/evo_buildHotkeysConfig.lua
@@ -1,4 +1,33 @@
 nameToKeyCode = {
+	ehbotfac = {102, 98},
+	eminifac = {102, 116},
+	ebasefactory = {102, 104},
+	eamphibfac = {102, 97},
+	eairplant = {102, 112},
+	
+	etech1 = {98, 116},
+	emetalextractor = {101, 109},
+	esolar2 = {101, 115},
+	egeothermal = {101, 103},
+	efusion2 = {101, 102},
+	estorage = {101, 100},
+	
+	eradar2 = {98, 114},
+	ejammer2 = {98, 106},
+	ekmar = {98, 107},
+	emine = {118, 109},
+	elightturret2 = {118, 112},
+	eheavyturret2 = {118, 105},
+	elaserbattery = {118, 98},
+	eartyturret = {118, 108},
+	eshieldgen = {118, 97},
+	esilo = {118, 115},
+	escoutdrone = {98, 100},
+	ebox = {98, 120},
+	ehbotengineer = {98, 101},
+	eorb = {98, 111},
+	ecommander = {118, 111},
+	
+	
 	elighttank3 = {98, 113},
-	etech1 = {98, 97},
 }
