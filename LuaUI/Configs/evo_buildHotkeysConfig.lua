9a4cd7ff9362dd12569cb823c06689b67f402932
CommonPlayer <thedd5d2092d50@yahoo.com.hk>
2018-10-28 16:26:43 +0800

    more hotkey ideas

--- LuaUI/Configs/evo_buildHotkeysConfig.lua
+++ LuaUI/Configs/evo_buildHotkeysConfig.lua
@@ -4,14 +4,14 @@ nameToKeyCode = {
 	ebasefactory = {102, 104},
 	eamphibfac = {102, 97},
 	eairplant = {102, 112},
-	
+
 	etech1 = {98, 116},
 	emetalextractor = {101, 109},
 	esolar2 = {101, 115},
 	egeothermal = {101, 103},
 	efusion2 = {101, 102},
 	estorage = {101, 100},
-	
+
 	eradar2 = {98, 114},
 	ejammer2 = {98, 106},
 	ekmar = {98, 107},
@@ -27,7 +27,56 @@ nameToKeyCode = {
 	ehbotengineer = {98, 101},
 	eorb = {98, 111},
 	ecommander = {118, 111},
-	
-	
-	elighttank3 = {98, 113},
+
+	-- HBots
+	ehbotpeewee     = {113}, -- Q
+	ehbotthud       = {119}, -- W
+	ehbotsniper     = {101}, -- E
+	ehbotrocko      = {114}, -- R
+	ehbotkarganneth = {116}, -- T
+	ehbotpeewee     = {121}, -- Y
+	ehbotthud       = {117}, -- U
+	ehbotsniper     = {105}, -- I
+	ehbotrocko      = {111}, -- O
+	ehbotkarganneth = {112}, -- P
+
+	-- ATs
+	eallterrlight    = {113}, -- Q
+	eallterrriot     = {119}, -- W
+	eallterrmed      = {101}, -- E
+	eallterrheavy    = {114}, -- R
+	eallterrassault  = {116}, -- T
+	eallterranarchid = {121}, -- Y
+	eallterrshield   = {117}, -- U
+
+	-- Hovers
+	elighttank3  = {113}, -- Q
+	eriottank2   = {119}, -- W
+	eheavytank3  = {101}, -- E
+	emissiletank = {114}, -- R
+	eartytank    = {116}, -- T
+	elacerator   = {121}, -- Y
+
+	-- Amphibious
+	eamphibbuggy   = {113}, -- Q
+	eamphibriot    = {119}, -- W
+	eamphibneedle  = {101}, -- E
+	eamphibmedtank = {114}, -- R
+	eamphibrock    = {116}, -- T
+	eamphibarty    = {121}, -- Y
+	eamphibleveler = {117}, -- U
+
+	-- Air
+	efighter   = {113}, -- Q
+	egunship2  = {119}, -- W
+	ebomber    = {101}, -- E
+	eairemp    = {114}, -- R
+	etransport = {116}, -- T
+	ekrow      = {121}, -- Y
+	efighter   = {117}, -- U
+	egunship2  = {105}, -- I
+	ebomber    = {111}, -- O
+	eairemp    = {112}, -- P
+	etransport = {97},  -- A
+	ekrow      = {115}, -- S
 }
