ef15ce04969ab4236cee45ea8868f7b71f7c3a04
Forboding Angel <forbodingangel@gmail.com>
2019-06-17 20:44:57 -0700

    Remove Chicken Defense completely

--- LuaAI.lua
+++ LuaAI.lua
@@ -12,60 +12,6 @@ return {
     name = 'DAI',
     desc = 'Shard by AF for Spring Lua',
   },
-
-  {
-    name = 'Chicken: Very Easy',
-    desc = 'Trivial Games'
-  },
-  {
-    name = 'Chicken: Easy',
-    desc = 'Normal Games'
-  },
-  {
-    name = 'Chicken: Normal',
-    desc = 'Average Games'
-  },
-  {
-    name = 'Chicken: Hard',
-    desc = 'Large Games'
-  },
-  {
-    name = 'Chicken: Very Hard',
-    desc = 'Hardcore Games'
-  },
-  {
-    name = 'Chicken: Epic!',
-    desc = 'Extreme Survival Games'
-  },
-  {
-    name = 'Chicken: Custom',
-    desc = 'Settings in ModOptions'
-  },
-  {
-    name = 'Chicken: Survival',
-    desc = 'Infinite Games'
-  },
-  
---[[  {
-    name = 'Survival Spawner: Very Easy',
-    desc = 'Beginner Games'
-  },
-  {
-    name = 'Survival Spawner: Easy',
-    desc = 'Normal Games'
-  },
-  {
-    name = 'Survival Spawner: Normal',
-    desc = 'Average Games'
-  },
-  {
-    name = 'Survival Spawner: Hard',
-    desc = 'Large Games'
-  },
-  {
-    name = 'Survival Spawner: Very Hard',
-    desc = 'Hardcore Games'
-  }, ]]--
 }
 
 
