781951f372efd667623bc8a37762a6fa41f866ce
Forboding Angel <forbodingangel@gmail.com>
2021-01-24 04:43:06 -0800

    Add file

---
+++ Weapons/NOWEAPON.lua
@@ -0,0 +1,14 @@
+unitDeaths = {
+
+	smallBuildingExplosionGeneric = {
+		weaponType		   = "Cannon",
+		impulseFactor      = 0,
+		AreaOfEffect=0,
+		explosiongenerator="custom:empty",
+		cameraShake=0,
+		damage = {
+			default            = 0,
+		},
+	},
+}
+return lowerkeys(unitDeaths)
