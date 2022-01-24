984b73804d5df22b1a1196b87c650ee830bb5fe4
Forboding Angel <forbodingangel@gmail.com>
2022-01-23 21:12:28 -0800

    v17.00

--- modinfo.lua
+++ modinfo.lua
@@ -13,7 +13,7 @@ return {
 	name='Evolution RTS -',
 	description='EvoRTS',
 	shortname='EvoRTS',
-	version='dev',
+	version='v17.00',
 	mutator='Official',
 	game='Evolution RTS',
 	shortGame='EvoRTS',
